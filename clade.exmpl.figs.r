7961a2f4cb2f4b747fae0f33b9d13b77c8c9efe7
ahhurlbert <hurlbert@bio.unc.edu>
2012-11-06 14:04:12 -0500

    added checks on minimum data for plotting

--- clade.exmpl.figs.r
+++ clade.exmpl.figs.r
@@ -18,9 +18,11 @@ clade.exmpl.figs = function(sim.results, clade.slices=6, time.buffer=20) {
   root.dist <- all.dist[length(phylo.out$tip.label)+1, ]
   rootdist = data.frame(clade.id = names(root.dist),time.of.origin = root.dist)
     
-  clade.time.slices = round(seq(max.time/clade.slices, max.time,by=max.time/clade.slices),0)
+  clade.time.slices = round(seq(max.time/(clade.slices+1), max.time - max.time/(clade.slices+1),length.out=clade.slices),0)
 
-  clades = sapply(clade.time.slices, function(x) {set.seed(1); sample(rootdist$clade.id[abs(rootdist$time.of.origin - x) < time.buffer],1)})
+  #Choose a clade randomly from those that are closest to the specified clade time slice
+  clades = sapply(clade.time.slices, function(x) {set.seed(1); 
+                  sample(rootdist$clade.id[abs(rootdist$time.of.origin - x) == min(abs(rootdist$time.of.origin - x))],1)})
   clds = as.numeric(as.character(clades))
 
   pdf(paste('clade_example_figs_sim',sim.params[1,1],'.pdf',sep=''),width=9,height=6)
@@ -31,19 +33,53 @@ clade.exmpl.figs = function(sim.results, clade.slices=6, time.buffer=20) {
     cl.analysis = regional.calc(cl.subset[cl.subset<11,c('region','spp.name','time.of.origin')], phylo.out, max.time)
     attach(cl.analysis)
 
-    if(
-    plot(richness~region, xlab="Environment",ylab="Species Richness", cex=3, pch=16, col='darkblue', xlim=c(1,10))
-    legend("top",legend=paste('r =',round(cor(richness,region,use="complete.obs"),2)), bty='n')
-    plot(richness~MRD, xlab = "MRD", ylab="Species Richness", cex=3, pch=15, col = 'skyblue')
-    legend("top",legend=paste('r =',round(cor(richness,MRD,use="complete.obs"),2)), bty='n')
-    plot(richness~PSV, xlab = "PSV", ylab="Species Richness", cex=3, pch=17, col = 'darkgreen')
-    legend("top",legend=paste('r =',round(cor(richness,PSV,use="complete.obs"),2)), bty='n')
-    plot(richness~time.in.region, xlab = "Time in Region", ylab="Species Richness", cex=3, pch=17, col = 'yellow3')
-    legend("top",legend=paste('r =',round(cor(richness,time.in.region,use="complete.obs"),2)), bty='n')
-    plot(MRD~region, ylab = "MRD", xlab="Environment", cex=3, pch=18, col = 'orange', xlim=c(1,10))
-    legend("top",legend=paste('r =',round(cor(region,MRD,use="complete.obs"),2)), bty='n')
-    plot(PSV~region, ylab = "PSV", xlab="Environment", cex=3, pch=15, col = 'darkred', xlim=c(1,10))
-    legend("top",legend=paste('r =',round(cor(region,PSV,use="complete.obs"),2)), bty='n')
+    if(length(unique(richness[!is.na(richness)])) > 2) {
+      plot(richness~region, xlab="Environment",ylab="Species Richness", cex=3, pch=16, col='darkblue', xlim=c(1,10))
+      legend("top",legend=paste('r =',round(cor(richness,region,use="complete.obs"),2)), bty='n')
+    } else {
+      plot(1,1, xlab="Environment",ylab="Species Richness", type="n")
+      text(1,1,"NA")
+    }
+    
+    if(length(unique(MRD[!is.na(MRD)])) > 2) {
+      plot(richness~MRD, xlab = "MRD", ylab="Species Richness", cex=3, pch=15, col = 'skyblue')
+      legend("top",legend=paste('r =',round(cor(richness,MRD,use="complete.obs"),2)), bty='n')
+    } else {
+      plot(1,1, xlab="MRD",ylab="Species Richness", type="n")
+      text(1,1,"NA")
+    }
+
+    if(length(unique(PSV[!is.na(PSV)])) > 2) {
+      plot(richness~PSV, xlab = "PSV", ylab="Species Richness", cex=3, pch=17, col = 'darkgreen')
+      legend("top",legend=paste('r =',round(cor(richness,PSV,use="complete.obs"),2)), bty='n')
+    } else {
+      plot(1,1, xlab="PSV",ylab="Species Richness", type="n")
+      text(1,1,"NA")    
+    }
+    
+    if(length(unique(richness[!is.na(richness)])) > 2) {
+      plot(richness~time.in.region, xlab = "Time in Region", ylab="Species Richness", cex=3, pch=17, col = 'yellow3')
+      legend("top",legend=paste('r =',round(cor(richness,time.in.region,use="complete.obs"),2)), bty='n')
+    } else {
+      plot(1,1, xlab="Time in Region",ylab="Species Richness", type="n")
+      text(1,1,"NA")
+    }
+    
+    if(length(unique(MRD[!is.na(MRD)])) > 2) {
+      plot(MRD~region, ylab = "MRD", xlab="Environment", cex=3, pch=18, col = 'orange', xlim=c(1,10))
+      legend("top",legend=paste('r =',round(cor(region,MRD,use="complete.obs"),2)), bty='n')
+    } else {
+      plot(1,1, xlab="Environment",ylab="MRD", type="n")
+      text(1,1,"NA")
+    }
+    
+    if(length(unique(MRD[!is.na(MRD)])) > 2) {
+      plot(PSV~region, ylab = "PSV", xlab="Environment", cex=3, pch=15, col = 'darkred', xlim=c(1,10))
+      legend("top",legend=paste('r =',round(cor(region,PSV,use="complete.obs"),2)), bty='n')
+    } else {
+      plot(1,1, xlab="Environment",ylab="PSV", type="n")
+      text(1,1,"NA")
+    }
 
     if (sim.params[8,1]==1 & sim.params[9,1]==1) {
       K.text = 'K gradient present'
@@ -55,7 +91,7 @@ clade.exmpl.figs = function(sim.results, clade.slices=6, time.buffer=20) {
     mtext(paste('Sim',sim.params[1,1],', Origin =',sim.params[3,1],', w =',sim.params[4,1],', sigma =',sim.params[7,1],
                 ',disp = ',sim.params[6,1],', specn =',sim.params[5,1],',',K.text),outer=T,line=2)
     
-    mtext(paste("CladeID =",i,"; Clade time of origin =", rootdist[rootdist$clade.id==i,'RD'],"; Clade richness =",length(cl.members)),3,outer=T,line=.3)
+    mtext(paste("CladeID =",i,"; Clade time of origin =", rootdist[rootdist$clade.id==i,'time.of.origin'],"; Clade richness =",length(cl.members)),3,outer=T,line=.3)
     detach(cl.analysis)
   }
   dev.off()
