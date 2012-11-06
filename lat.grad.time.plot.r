ae4b485b8c853f956dac50f5d2c5892ffd111949
ahhurlbert <hurlbert@bio.unc.edu>
2012-11-06 17:29:25 -0500

    fixed mtext label

--- lat.grad.time.plot.r
+++ lat.grad.time.plot.r
@@ -14,20 +14,20 @@ lat.grad.time.plot = function(sim.results, numslices) {
   slices = round(seq(max.time/numslices, max.time,by=max.time/numslices),0)
   cols = rainbow(numslices+1)
 
-  pdf(paste('lat_grad_thru_time_sim',sim,'.pdf',sep=''),height=6,width=8)
+  pdf(paste('lat_grad_thru_time_sim',sim.params[1,1],'.pdf',sep=''),height=6,width=8)
   par(mfrow=c(1,1),oma=c(0,0,2.5,0),mar=c(4,4,1,1))
   plot(c(1,10),c(0,max(rich.time2$spp.rich)),type="n",xlab="Latitude",ylab="Species richness")
   sapply(slices,function(x) points(11 - rich.time2$region[rich.time2$time==x], rich.time2$spp.rich[rich.time2$time==x],type='l',col=cols[which(slices==x)]))
   legend('topright',legend=slices,lty='solid',col=cols[2:(numslices+1)])
-  if (sim.params[8,1]==1 & sim.params[9,1]==1) {
+  if (sim.params$carry.cap=='on' & sim.params$energy.gradient=='on') {
     K.text = 'K gradient present'
-  } else if (sim.params[8,1]==1 & sim.params[9,1]==2) {
+  } else if (sim.params$carry.cap=='on' & sim.params$energy.gradient=='off') {
     K.text = 'K constant across regions'
-  } else if (sim.params[8,1]==2) {
+  } else if (sim.params$carry.cap=='off') {
     K.text = 'no K'
   }
-  mtext(paste('Sim',sim.params[1,1],', Origin =',sim.params[3,1],', w =',sim.params[4,1],', sigma =',sim.params[7,1],
-              ',\ndisp = ',sim.params[6,1],', specn =',sim.params[5,1],',',K.text),outer=T)
+  mtext(paste('Sim',sim.params[1,1],', Origin =',sim.params[1,3],', w =',sim.params[1,4],', sigma =',sim.params[1,7],
+              ',\ndisp = ',sim.params[1,6],', specn =',sim.params[1,5],',',K.text),outer=T)
   dev.off()
 }
 
