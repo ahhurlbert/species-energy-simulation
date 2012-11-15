0d935e39f6316d80a195bc539243262c1550aab9
ahhurlbert <hurlbert@bio.unc.edu>
2012-11-15 15:21:39 -0500

    will only consider clades w/ >10 spp, added extinction rate plots

--- clade.exmpl.figs.r
+++ clade.exmpl.figs.r
@@ -2,38 +2,36 @@
 # The 'time.buffer' is the potential +/- variation (in time units) allowed in selecting clades of a specific age
 
 
-clade.exmpl.figs = function(sim.results, clade.slices=6, seed=1) {
+clade.exmpl.figs = function(sim.results, reg.stats, clade.slices=6, seed=1) {
+  require(caper)
+  
   all.pops = sim.results$all.populations
   phylo.out = sim.results$phylo.out
   max.time = as.integer(max(all.pops$time.of.origin))
   sim.params = sim.results$params.out
-
+  stats = reg.stats
+ 
+  #Clade origin times (i.e. root distances)
+  # --only focus on clades w/ at least 10 species for analysis
+  rootdist = stats[stats$richness >= 10, c('clade.id','clade.origin.time')]
   
-  ###############################################################################
-  # Replace code here in these next 8 lines as necessary to incorporate correct
-  # values for clade time of origin...
-  ####################################
-  #Node origin times based on phylogeny at end of simulation
-  all.dist <- dist.nodes(phylo.out)
-  root.dist <- all.dist[length(phylo.out$tip.label)+1, ]
-  rootdist = data.frame(clade.id = names(root.dist),time.of.origin = root.dist)
-    
   clade.time.slices = round(seq(max.time/(clade.slices+1), max.time - max.time/(clade.slices+1),length.out=clade.slices),0)
 
-  #Choose a clade randomly from those that are closest to the specified clade time slice,
-  #specifically those that are within 10 time units of the closest clade
-  clades = sapply(clade.time.slices, function(x) {set.seed(seed); 
-                  sample(rootdist$clade.id[abs(rootdist$time.of.origin - x) < (min(abs(rootdist$time.of.origin - x))+10)],1)})
-  clds = as.numeric(as.character(clades))
-
-  pdf(paste('clade_example_figs_sim',sim.params[1,1],'.pdf',sep=''),width=9,height=6)
-  par(mfrow=c(2,3),oma = c(1,1,4,0),mar=c(4,4,1,1))
-  for (i in clds) {
-    cl.members = clade.members(i,phylo.out)
+  pdf(paste('clade_example_figs_sim',sim.params[1,1],'.pdf',sep=''),width=10,height=9)
+  par(mfrow=c(3,3),oma = c(1,1,4,0),mar=c(4,4,1,1))
+  
+  for (c in 1:clade.slices) {
+  
+    #Choose a clade randomly from those that are closest to the specified clade time slice,
+    #specifically those that are within 100 time units of the closest clade
+    set.seed(seed)
+    clade = sample(rootdist$clade.id[abs(rootdist$clade.origin.time - clade.time.slices[c]) < 
+                                     (min(abs(rootdist$clade.origin.time - clade.time.slices[c]))+100)],1)
+    cl.members = clade.members(which(phylo.out$tip.label==clade),phylo.out)
     cl.subset = subset(all.pops, spp.name %in% cl.members)
     cl.analysis = regional.calc(cl.subset[c('region','spp.name','time.of.origin','reg.env','extant')], phylo.out, max.time)
+    
     attach(cl.analysis)
-
     if(length(unique(richness[!is.na(richness)])) > 2) {
       plot(richness~reg.env, xlab="Environment",ylab="Species Richness", cex=3, pch=16, col='darkblue',xlim=range(sim.results$all.populations$reg.env))
       legend("top",legend=paste('r =',round(cor(richness,reg.env,use="complete.obs"),2)), bty='n')
@@ -82,6 +80,22 @@ clade.exmpl.figs = function(sim.results, clade.slices=6, seed=1) {
       text(1,1,"NA")
     }
 
+    if(length(unique(richness[!is.na(richness)])) > 2) {
+      plot(richness~extinction.rate, ylab = "Species Richness", xlab="Extinction Rate", cex=3, pch=16, col = 'purple3')
+      legend("top",legend=paste('r =',round(cor(richness,extinction.rate,use="complete.obs"),2)), bty='n')
+    } else {
+      plot(1,1, xlab="Extinction Rate",ylab="Species Richness", type="n")
+      text(1,1,"NA")
+    }
+    
+    if(length(unique(extinction.rate[!is.na(extinction.rate)])) > 2) {
+      plot(extinction.rate~region, ylab = "Extinction Rate", xlab="Region", cex=3, pch=18, col = 'pink')
+      legend("top",legend=paste('r =',round(cor(region,extinction.rate,use="complete.obs"),2)), bty='n')
+    } else {
+      plot(1,1, xlab="Region",ylab="Extinction Rate", type="n")
+      text(1,1,"NA")
+    }
+    
     if (sim.params$carry.cap=='on' & sim.params$energy.gradient=='on') {
       K.text = 'K gradient present'
     } else if (sim.params$carry.cap=='on' & sim.params$energy.gradient=='off') {
