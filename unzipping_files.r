e6fa0309c8a3dd8676042a3816738cc8fcf1b7e8
ahhurlbert <hurlbert@bio.unc.edu>
2012-12-03 09:59:56 -0500

    function for unzipping output files

--- unzipping_files.r
+++ unzipping_files.r
@@ -1,5 +1,12 @@
-unzipped.filenames = unzip('C:/SENCoutput/senc.out.1.zip')
-all.pops = read.csv(unzipped.filenames[1], header=T)
-params.out = read.csv(unzipped.filenames[2],header=T)
-phylo.out = read.tree(unzipped.filenames[3], header=T)
-time.rich = read.tree(unzipped.filenames[4], header=T)
+
+#Function for unzipping output files
+# --output_dir is the directory where the zipped output files reside
+
+output.unzip = function(output_dir, sim_ID) {
+  unzipped.filenames = unzip(paste(output_dir,'/senc.out.',sim_ID,'.zip',sep=''))
+  all.populations = read.csv(unzipped.filenames[1], header=T)
+  params.out = read.csv(unzipped.filenames[2],header=T)
+  phylo.out = read.tree(unzipped.filenames[3])
+  time.richness = read.csv(unzipped.filenames[4], header=T)
+}
+
