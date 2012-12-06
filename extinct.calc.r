4378b7869745fc14721b43665e8e42194dea084a
ahhurlbert <hurlbert@bio.unc.edu>
2012-12-06 15:32:09 -0500

    changed time.of.origin < timeslice to time.of.origin <=timeslice; this prevents the function from failing on small recent clades where all of the species arose in the final time step

--- extinct.calc.r
+++ extinct.calc.r
@@ -9,7 +9,7 @@
 # The argument should be an all.populations dataframe subsetted down to the appropriate
 # timeslice (i.e., without species with time.of.origin > t)
 extinct.calc = function(all.populations, timeslice) {
-  sub.populations = subset(all.populations, time.of.origin < timeslice)
+  sub.populations = subset(all.populations, time.of.origin <= timeslice)
   num.all.lineages = data.frame(table(sub.populations$region))
   extinct.lineages = aggregate(sub.populations$time.of.extinction,by=list(sub.populations$region), function(x) sum(x<timeslice))
   time.col = aggregate(sub.populations$time.of.origin,by=list(sub.populations$region), min)
