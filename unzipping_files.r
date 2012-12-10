167916813715d85d2890152a0e3303a0b539b132
ahhurlbert <hurlbert@bio.unc.edu>
2012-12-10 09:15:57 -0500

    added a file existence check to this function; it now returns a NULL value if file does not exist

--- unzipping_files.r
+++ unzipping_files.r
@@ -3,15 +3,20 @@
 # --output_dir is the directory where the zipped output files reside
 
 output.unzip = function(sim_dir, sim_ID) {
-  unzipped.filenames = unzip(paste(sim_dir,'/senc.out.',sim_ID,'.zip',sep=''), exdir = sim_dir)
-  all.populations = read.csv(unzipped.filenames[1], header=T)
-  params.out = read.csv(unzipped.filenames[2],header=T)
-  phylo.out = read.tree(unzipped.filenames[3])
-  time.richness = read.csv(unzipped.filenames[4], header=T)
-  sim.results = list(all.populations=all.populations, 
-                     params.out=params.out, 
-                     phylo.out=phylo.out, 
-                     time.richness=time.richness)
-  return(sim.results)
+  
+  #check that file exists (returns value of 0 when mode=0 if file exists)
+  if( file.access(paste(sim_dir,'/senc.out.',sim_ID,'.zip',sep=''), mode=0) == 0 ) {
+    unzipped.filenames = unzip(paste(sim_dir,'/senc.out.',sim_ID,'.zip',sep=''), exdir = sim_dir)
+    all.populations = read.csv(unzipped.filenames[1], header=T)
+    params.out = read.csv(unzipped.filenames[2],header=T)
+    phylo.out = read.tree(unzipped.filenames[3])
+    time.richness = read.csv(unzipped.filenames[4], header=T)
+    sim.results = list(all.populations=all.populations, 
+                       params.out=params.out, 
+                       phylo.out=phylo.out, 
+                       time.richness=time.richness)
+    return(sim.results)
+  } else { 
+    return(NULL)}
 }
 
