2929eda798cddd69fa50d87b7deb29663269447b
ahhurlbert <hurlbert@bio.unc.edu>
2012-12-13 10:25:55 -0500

    script for calculating Blombergs K from simulation output (all.pops and phylo.out); note that this is now being done in the analysis_workflow_Olympus.r script, and should only be necessary to supplement output generated prior to 12 December 2012

---
+++ BlomK.stats.quickfix.r
@@ -0,0 +1,57 @@
+# Function for calculating Blomberg's K from the simulation files all.pops and phylo.out.
+# Note that Blomberg's K is calculated as part of the stats output based on 
+# analysis_workflow_Olympus.r as of 12/12/12.
+
+# This script is being used to go through files retroactively in the event that K wasn't 
+# calculated before.
+
+file_dir = 'C:/SENCoutput'
+
+
+BlomK.stats = function(file_dir, t=30000) {
+  require(ape)
+  
+  files = list.files(file_dir)
+  allpops.files = files[grep('all.pops',files)]
+  phylo.files = files[grep('phylo',files)]
+  
+  sims = as.vector( sapply(allpops.files, function(x)
+    as.numeric(strsplit(strsplit(x, "sim")[[1]][2] , "\\.csv")[[1]][1])) )
+  
+  sim.BK = data.frame(sim = NA, BK.env = NA, BK.reg = NA)
+  
+  for (i in sims) {
+    pops = read.csv(paste(file_dir,'/SENC_all.pops_sim',i,'.csv',sep=''), header=T)
+    phy = read.tree(paste(file_dir,'/SENC_phylo_sim',i,'.tre',sep=''))
+    if (nrow(pops) > 0) {
+    
+      # sub.pops contains only extant species
+      sub.pops = subset(pops, extant==1)
+      
+      # drop extinct species from phy (note: drop.tip is much faster than prune.extinct.taxa)
+      tips.to.drop = as.character(phy$tip.label[!phy$tip.label %in% as.character(sub.pops$spp.name)]);
+      phy2 = drop.tip( phy , tips.to.drop );
+      
+      # Change any tips of branch length 0 to branch length 1; otherwise phylosig will bonk
+      phy2$edge.length[phy2$edge.length==0] = 1
+      
+      # Calculate Blomberg's K for two traits: environmental optimum, and mean region of occurrence
+      spp.traits = aggregate(sub.pops$region, by = list(sub.pops$spp.name, sub.pops$env.opt),
+                             function(x) mean(x, na.rm=T))
+      names(spp.traits) = c('spp.name','env.opt','region')
+        
+      spp.env = spp.traits$env.opt
+      names(spp.env) = spp.traits$spp.name
+      BK.env = phylosig(phy2, spp.env[phy2$tip.label], method="K")
+        
+      spp.reg = spp.traits$region
+      names(spp.reg) = spp.traits$spp.name
+      BK.reg = phylosig(phy2, spp.reg[phy2$tip.label], method="K")
+        
+      sim.BK = rbind(sim.BK, cbind(sim = i, BK.env = BK.env, BK.reg = BK.reg))
+      print(paste(i, date(), sep="   "))
+    } # end if
+  } # end sim loop
+  sim.BK = sim.BK[-1,]
+  return(sim.BK)
+}
