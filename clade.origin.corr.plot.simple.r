38cba9c55b5106fd175e5c5e60e30e88dd2e9dab
ahhurlbert <hurlbert@bio.unc.edu>
2013-01-28 15:06:36 -0500

    unknown edits

--- clade.origin.corr.plot.simple.r
+++ clade.origin.corr.plot.simple.r
@@ -23,20 +23,20 @@ clade.origin.corr.plot.simple = function(stats.output,
     spline.df = 4
     
     if (length(x$r.env.rich[!is.na(x$r.env.rich)]) > min.num.data.pts) {
-      plot(x$clade.origin.time, x$r.env.rich, xlab="",ylab="r (Env-Richness)",ylim=c(-1,1))
+      plot(log10(x$clade.origin.time), x$r.env.rich, xlab="",ylab="r (Env-Richness)",ylim=c(-1,1))
       rect(-1000,.5,t+50,1.1,col=rgb(.1,.1,.1,.1),lty=0)
-      points(smooth.spline(x$clade.origin.time[!is.na(x$r.env.rich)],x$r.env.rich[!is.na(x$r.env.rich)],df=spline.df),
+      points(smooth.spline(log10(x$clade.origin.time[!is.na(x$r.env.rich)]),x$r.env.rich[!is.na(x$r.env.rich)],df=spline.df),
         type='l',col='red')
-      points(clade.origin$clade.origin.time, clade.origin$r.env.rich, col = 'skyblue', pch = 17, cex = 2)
+      points(log10(clade.origin$clade.origin.time), clade.origin$r.env.rich, col = 'skyblue', pch = 17, cex = 2)
     } else {
       plot(1,1,xlab="",ylab = "r (Env-Richness)",type="n",xlim=c(0,t),ylim=c(-1,1))
     }
     
     if (length(x$r.time.rich[!is.na(x$r.time.rich)]) > min.num.data.pts) {
-      plot(x$clade.origin.time, x$r.time.rich, xlab = "",ylab="r (Time-Richness)",ylim=c(-1,1),main=paste('w =',sim.params[1,4],', sigma =',sim.params[1,7],sep=""))
+      plot(log10(x$clade.origin.time), x$r.time.rich, xlab = "",ylab="r (Time-Richness)",ylim=c(-1,1),main=paste('w =',sim.params[1,4],', sigma =',sim.params[1,7],sep=""))
       rect(-1000,.5,t+50,1.1,col=rgb(.1,.1,.1,.1),lty=0)
-      points(smooth.spline(x$clade.origin.time[!is.na(x$r.time.rich)],x$r.time.rich[!is.na(x$r.time.rich)],df=spline.df),type='l',col='red')
-      points(clade.origin$clade.origin.time, clade.origin$r.time.rich, col = 'skyblue', pch = 17, cex = 2)
+      points(smooth.spline(log10(x$clade.origin.time[!is.na(x$r.time.rich)]),x$r.time.rich[!is.na(x$r.time.rich)],df=spline.df),type='l',col='red')
+      points(log10(clade.origin$clade.origin.time), clade.origin$r.time.rich, col = 'skyblue', pch = 17, cex = 2)
     } else { 
       plot(1,1,xlab="",ylab = "r (Time-Richness)",type="n",xlim=c(0,t),ylim=c(-1,1))
     }
@@ -52,10 +52,10 @@ clade.origin.corr.plot.simple = function(stats.output,
     mtext(paste('Origin =',sim.params[1,3],', ndisp = ',sim.params[1,6],', specn =',sim.params[1,5],',',K.text),outer=T)
   
     if (length(x$gamma.stat[!is.na(x$gamma.stat)]) > min.num.data.pts) {
-      plot(x$clade.origin.time, x$gamma.stat, xlab = "", ylab="Gamma", ylim = c(-20,3))
+      plot(log10(x$clade.origin.time), x$gamma.stat, xlab = "", ylab="Gamma", ylim = c(-25,3))
       rect(-1000,-1.645,t+50,1.1,col=rgb(.1,.1,.1,.1),lty=0) # -1.645 is the critical value for rejecting constant rates
-      points(smooth.spline(x$clade.origin.time[!is.na(x$gamma.stat)],x$gamma.stat[!is.na(x$gamma.stat)],df=spline.df),type='l',col='red')
-      points(clade.origin$clade.origin.time, clade.origin$gamma.stat, col = 'skyblue', pch = 17, cex = 2)
+      points(smooth.spline(log10(x$clade.origin.time[!is.na(x$gamma.stat)]),x$gamma.stat[!is.na(x$gamma.stat)],df=spline.df),type='l',col='red')
+      points(log10(clade.origin$clade.origin.time), clade.origin$gamma.stat, col = 'skyblue', pch = 17, cex = 2)
     } else {
       plot(1,1,xlab="",ylab = "Gamma",type="n",xlim=c(0,t),ylim=c(-1,1))
     }
