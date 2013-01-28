4014634e36801de175314900ed71648cbd47c3fe
ahhurlbert <hurlbert@bio.unc.edu>
2013-01-28 09:54:38 -0500

    added blue symbol to plots to indicate the overall ancestral clade; i did this by selecting the clade with the highest clade richness, but this is not always the clade with the earliest origin time, so need to figure this out; also changed ylim for gamma plot

--- clade.origin.corr.plot.simple.r
+++ clade.origin.corr.plot.simple.r
@@ -17,25 +17,28 @@ clade.origin.corr.plot.simple = function(stats.output,
     #t = max(stats.output$time)                 #comment this out if looping over multiple time slices
     t = 30000
     x = subset(stats.output, n.regions >= min.num.regions & clade.richness >= min.num.spp.per.clade, select = 2:ncol(stats.output))
+    clade.origin = subset(x, clade.richness == max(x$clade.richness))
     spline.df = 4
     
     if (length(x$r.env.rich[!is.na(x$r.env.rich)]) > min.num.data.pts) {
       plot(x$clade.origin.time, x$r.env.rich, xlab="",ylab="r (Env-Richness)",ylim=c(-1,1))
+      rect(-1000,.5,t+50,1.1,col=rgb(.1,.1,.1,.1),lty=0)
       points(smooth.spline(x$clade.origin.time[!is.na(x$r.env.rich)],x$r.env.rich[!is.na(x$r.env.rich)],df=spline.df),
         type='l',col='red')
+      points(clade.origin$clade.origin.time, clade.origin$r.env.rich, col = 'skyblue', pch = 17, cex = 2)
     } else {
       plot(1,1,xlab="",ylab = "r (Env-Richness)",type="n",xlim=c(0,t),ylim=c(-1,1))
     }
-    rect(-1000,.5,t+50,1.1,col=rgb(.1,.1,.1,.1),lty=0)
-
+    
     if (length(x$r.time.rich[!is.na(x$r.time.rich)]) > min.num.data.pts) {
       plot(x$clade.origin.time, x$r.time.rich, xlab = "",ylab="r (Time-Richness)",ylim=c(-1,1),main=paste('w =',sim.params[1,4],', sigma =',sim.params[1,7],sep=""))
+      rect(-1000,.5,t+50,1.1,col=rgb(.1,.1,.1,.1),lty=0)
       points(smooth.spline(x$clade.origin.time[!is.na(x$r.time.rich)],x$r.time.rich[!is.na(x$r.time.rich)],df=spline.df),type='l',col='red')
+      points(clade.origin$clade.origin.time, clade.origin$r.time.rich, col = 'skyblue', pch = 17, cex = 2)
     } else { 
       plot(1,1,xlab="",ylab = "r (Time-Richness)",type="n",xlim=c(0,t),ylim=c(-1,1))
     }
-    rect(-1000,.5,t+50,1.1,col=rgb(.1,.1,.1,.1),lty=0)
-
+    
     mtext("Clade origin time",1,outer=T,line=.8)
     if (sim.params[1,8]=='on' & sim.params[1,9]=='on') {
       K.text = 'K gradient present'
@@ -47,15 +50,13 @@ clade.origin.corr.plot.simple = function(stats.output,
     mtext(paste('Origin =',sim.params[1,3],', ndisp = ',sim.params[1,6],', specn =',sim.params[1,5],',',K.text),outer=T)
   
     if (length(x$gamma.stat[!is.na(x$gamma.stat)]) > min.num.data.pts) {
-      plot(x$clade.origin.time, x$gamma.stat, xlab = "", ylab="Gamma", ylim = c(-15,3))
+      plot(x$clade.origin.time, x$gamma.stat, xlab = "", ylab="Gamma", ylim = c(-17,3))
+      rect(-1000,-1.645,t+50,1.1,col=rgb(.1,.1,.1,.1),lty=0) # -1.645 is the critical value for rejecting constant rates
       points(smooth.spline(x$clade.origin.time[!is.na(x$gamma.stat)],x$gamma.stat[!is.na(x$gamma.stat)],df=spline.df),type='l',col='red')
+      points(clade.origin$clade.origin.time, clade.origin$gamma.stat, col = 'skyblue', pch = 17, cex = 2)
     } else {
       plot(1,1,xlab="",ylab = "Gamma",type="n",xlim=c(0,t),ylim=c(-1,1))
     }
-    rect(-1000,-1.645,t+50,1.1,col=rgb(.1,.1,.1,.1),lty=0) # -1.645 is the critical value for rejecting constant rates
-
   #}
- 
-  
 }
 
