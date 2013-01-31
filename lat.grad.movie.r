b5954899c361989a0554b31ff48bb0896b13da95
ahhurlbert <hurlbert@bio.unc.edu>
2013-01-31 14:20:45 -0500

    function for creating a movie-plot in an R window showing the evolution of the latitudinal gradient through time at fine time resolutions

---
+++ lat.grad.movie.r
@@ -0,0 +1,23 @@
+lat.grad.movie = function(all.populations) {
+  timeslices = seq(10,10000,by=10)
+  time.window = timeslices[2] - timeslices[1]
+  max.rich = max(table(all.populations[all.populations$time.of.extinction > 30000,'region']))
+  
+  reg.rich.thru.time = data.frame(time=NA, region=NA, total.rich=NA)
+  par(mfrow = c(1,1))
+  for (t in timeslices) {
+    
+    all.pops = subset(all.populations, time.of.origin < t & time.of.extinction > t)
+    all.reg.rich = data.frame(table(all.pops$region))
+    names(all.reg.rich) = c('region','total.rich')
+    all.reg.rich$region = as.numeric(as.character(all.reg.rich$region))
+
+    plot(11 - all.reg.rich$region, log10(all.reg.rich$total.rich), type='b', lwd = 4, cex = .5, col = 'red',
+                                         xlim = c(0,11), ylim=c(0, log10(max.rich)), xlab="Latitude",ylab = "log10 Species richness")
+    text(10, log10(max.rich), paste("t =", t))
+    
+    reg.rich.thru.time = rbind(reg.rich.thru.time, cbind(time=rep(t,nrow(all.reg.rich)), all.reg.rich))
+    for (i in 1:1000000) {}
+  }
+
+}
