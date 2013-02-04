874684d13789c9d3b32ebd7501f73534f7c0b6c8
ahhurlbert <hurlbert@bio.unc.edu>
2013-02-04 09:19:47 -0500

    modified so the actual sim.matrix is an argument, not just the directory and filename; comments added

--- lat.grad.movie.r
+++ lat.grad.movie.r
@@ -4,9 +4,13 @@ source(paste(github_dir,'/unzipping_files.r',sep=''))
 sim_dir = 'c:/sencoutput/senc.out.130115' #directory with simulation output
 
 #sim_matrix = 'c:/sencoutput/senc.out.130115/sim.matrix.output_2013-01-16.csv'
-sim_matrix = '//bioark.bio.unc.edu/hurlbertallen/manuscripts/cladevscommunity/analyses/sim.matrix.output_2012-12-14.csv'
+sim.matrix = read.csv('//bioark.bio.unc.edu/hurlbertallen/manuscripts/cladevscommunity/analyses/sim.matrix.output_2012-12-14.csv',header=T)
 
-lat.grad.movie = function(sim, sim_matrix, sim_dir, time.step, time.max, unzip=T) {
+
+# Function that shows a time lapse movie of the development of the latitudinal gradient in species richness.
+# Arguments include the simID, sim.matrix, directory in which simulation files are stored, 
+# temporal resolution and duration of the movie, and whether the sim files need to be unzipped or not.
+lat.grad.movie = function(sim, sim.matrix, sim_dir, time.step, time.max, unzip=T) {
   
   if(unzip) {
     sim.out = output.unzip(sim_dir, sim)
@@ -14,7 +18,7 @@ lat.grad.movie = function(sim, sim_matrix, sim_dir, time.step, time.max, unzip=T
   } else { 
     all.populations = read.csv(paste(sim_dir,'/SENC_all.pops_sim',sim,'.csv',sep=''), header=T)
   }
-  sim.matrix = read.csv(sim_matrix,header=T)
+  
   params = sim.matrix[sim.matrix$sim.id==sim,]
   
   timeslices = seq(time.step, time.max, by=time.step)
@@ -31,9 +35,9 @@ lat.grad.movie = function(sim, sim_matrix, sim_dir, time.step, time.max, unzip=T
     all.reg.rich$region = as.numeric(as.character(all.reg.rich$region))
 
     plot(11 - all.reg.rich$region, log10(all.reg.rich$total.rich), type='b', lwd = 4, cex = .5, col = 'red',
-         xlim = c(0,11), ylim=c(0, log10(max.rich)), xlab="Latitude",ylab = "log10 Species richness",
-         main=paste(params[1,3],"origin; K", params[1,8]), "; w =",params[1,4],"; sigma = ",params[1,7])
-    text(10, log10(max.rich), paste("t =", t))
+         xlim = c(0,11), ylim=c(0, log10(max.rich)+.5), xlab="Latitude",ylab = "log10 Species richness",
+         main=paste(params[1,3],"origin; K", params[1,8], "; w =",params[1,4],"; sigma = ",params[1,7]))
+    text(10, log10(max.rich)+.5, paste("t =", t))
     
     reg.rich.thru.time = rbind(reg.rich.thru.time, cbind(time=rep(t,nrow(all.reg.rich)), all.reg.rich))
     for (i in 1:1000000) {}
