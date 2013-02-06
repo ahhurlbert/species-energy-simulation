d51986ba5c41d4ccf12972ce83ffdb7183b3d5a9
ahhurlbert <hurlbert@bio.unc.edu>
2013-02-06 18:48:04 -0500

    added second panel illustrating distributions of environmental optima across 4 regions and how those distributions shift through time

--- lat.grad.movie.comp.r
+++ lat.grad.movie.comp.r
@@ -39,7 +39,7 @@ lat.grad.movie.comp = function(sims, sim.matrix, sim_dir, time.step, time.max, u
                      table(all.populations2[all.populations2$time.of.extinction > 30000,'region'])))
   
   #reg.rich.thru.time = data.frame(time=NA, region=NA, total.rich=NA)
-  par(mfrow = c(1,1))
+  par(mfrow = c(2,1), mar = c(4,4,2,1), mgp = c(2.5,1,0))
   for (t in timeslices) {
     
     all.pops1 = subset(all.populations1, time.of.origin < t & time.of.extinction > t)
@@ -53,6 +53,7 @@ lat.grad.movie.comp = function(sims, sim.matrix, sim_dir, time.step, time.max, u
     all.reg.rich2$region = as.numeric(as.character(all.reg.rich2$region))
     
     Sys.sleep(0)
+    # Panel 1: richness gradient
     plot(11 - all.reg.rich1$region, log10(all.reg.rich1$total.rich), type='b', lwd = 4, cex = .5, col = 'red',
          xlim = c(0,11), ylim=c(0, log10(max.rich)+.5), xlab="Latitude",ylab = "log10 Species richness",
          main=paste(params1[1,3],"origin; K", params1[1,8], "; w =",params1[1,4],"; sigma = ",params1[1,7]))
@@ -61,6 +62,21 @@ lat.grad.movie.comp = function(sims, sim.matrix, sim_dir, time.step, time.max, u
     legend('topleft',c(paste('freq =', params1[1,15],'; magn =', params1[1,16]),
                               paste('freq =', params2[1,15],'; magn =', params2[1,16])), lwd=4, col = c('red','blue'))
     text(10, log10(max.rich)+.5, paste("t =", t))
+    
+    # Panel 2: environmental optima
+    regions = c(1,4,7,10)
+    
+    plot(1,1,type="n",xlim=c(-5,45), ylim = c(0,0.3), xlab="Thermal Optimum", ylab = "Density")
+    reg.env = unique(all.populations1[all.populations1$region %in% regions, c('region','reg.env')])
+    abline(v = reg.env$reg.env, lty="dotted")
+    for (r in regions) {
+      if (nrow(all.pops1[all.pops1$region==r,]) > 3) {
+        points(density(all.pops1[all.pops1$region==r, 'env.opt']), type = 'l', col = 'red')
+      }
+      if (nrow(all.pops2[all.pops2$region==r,]) > 3) {
+        points(density(all.pops2[all.pops2$region==r, 'env.opt']), type = 'l', col = 'blue')
+      }
+    }
     
     #reg.rich.thru.time = rbind(reg.rich.thru.time, cbind(time=rep(t,nrow(all.reg.rich)), all.reg.rich))
     for (i in 1:100000) {}
