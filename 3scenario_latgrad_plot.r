60ef28702b38a21550f7e36ddb31839818879f75
stegen <James.Stegen@pnnl.gov>
2013-03-06 12:08:14 -0800

    lat grad plot update

--- 3scenario_latgrad_plot.r
+++ 3scenario_latgrad_plot.r
@@ -1,9 +1,25 @@
 # Plot boxplots of gamma (phylogenetic tree shape statistic) across 3 scenarios
 # (K gradient, disturbance gradient, or no limit) and 2 regions of origin.
-sim_dir = "C:/SENCoutput"
-analysis_dir = "//bioark.bio.unc.edu/hurlbertallen/manuscripts/cladevscommunity/analyses/"
-repo_dir = "C:/Documents and Settings/Hurlbert/species-energy-simulation"
 
+Allen = 0;
+
+if (Allen ==1) {
+  
+  sim_dir = "C:/SENCoutput"
+  analysis_dir = "//bioark.bio.unc.edu/hurlbertallen/manuscripts/cladevscommunity/analyses/"
+  repo_dir = "C:/Documents and Settings/Hurlbert/species-energy-simulation"
+
+}
+
+if (Allen == 0) {
+  
+  sim_dir = "C:/Users/steg815/Desktop/Stegen_PNNL/Spp-Energy-Niche-Conserv/sims.out.130204"
+  analysis_dir = "C:/Users/steg815/Desktop/Stegen_PNNL/Spp-Energy-Niche-Conserv/sims.out.130204"
+  repo_dir = "C:/Users/steg815/Desktop/Stegen_PNNL/Spp-Energy-Niche-Conserv/species-energy-simulation"  
+  
+}
+
+  
 sim.matrix = read.csv(paste(repo_dir,'/SENC_Master_Simulation_Matrix.csv',sep=""),header=T)
 
 # Scenarios
@@ -63,73 +79,73 @@ oldgrad.NoLim.te = old.lat.grad(NoLim.te$sim.id, time = 140)
 pdf(paste(analysis_dir,'/summaryplots/3scenarios_lat_grads.pdf',sep=''), height = 4, width = 8)
 par(mfrow=c(1,3), mar = c(4,4,2,1), oma = c(2,1,1,1))
 ## Time scenario
-plot(c(1,10),c(0,max(latgrad.NoLim.tr$spp.rich)),type="n",xlab="Environmental Bin",ylab="Species Richness", main="Time",cex.axis=1.5)
+plot(c(1,10),c(0,max(latgrad.NoLim.tr$spp.rich)),type="n",xlab="Environmental Bin",ylab="Species Richness", main="Time",cex.lab=1.5)
 #recent gradient
 sapply(NoLim.tr$sim.id, function(x)
        points(11 - latgrad.NoLim.tr[latgrad.NoLim.tr$sim==x,'region'], 
               latgrad.NoLim.tr[latgrad.NoLim.tr$sim==x,'spp.rich'], 
-              type = 'l', lwd=2, col = 'gray80'))
+              type = 'l', lwd=2, col = 'red'))
 sapply(NoLim.te$sim.id, function(x)
   points(11 - latgrad.NoLim.te[latgrad.NoLim.te$sim==x,'region'], 
          latgrad.NoLim.te[latgrad.NoLim.te$sim==x,'spp.rich'], 
-         type = 'l', lwd=2, col = 'pink'))
+         type = 'l', lwd=2, col = 'blue'))
 #old gradient
 sapply(NoLim.tr$sim.id, function(x)
   points(11 - oldgrad.NoLim.tr[oldgrad.NoLim.tr$sim==x,'region'], 
          oldgrad.NoLim.tr[oldgrad.NoLim.tr$sim==x,'spp.rich'], 
-         type = 'l', lwd=2, col = 'gray40', lty='dashed'))
+         type = 'l', lwd=2, col = 'pink', lty='dashed'))
 sapply(NoLim.te$sim.id, function(x)
   points(11 - oldgrad.NoLim.te[oldgrad.NoLim.te$sim==x,'region'], 
          oldgrad.NoLim.te[oldgrad.NoLim.te$sim==x,'spp.rich'], 
-         type = 'l', lwd=2, col = 'indianred4', lty='dashed'))
+         type = 'l', lwd=2, col = 'light blue', lty='dashed'))
 
 ## Disturbance scenario
-plot(c(1,10),c(0,max(latgrad.dist.tr$spp.rich)),type="n",xlab="Latitude",
-     ylab="Species richness", main="Disturbance")
+plot(c(1,10),c(0,max(latgrad.dist.tr$spp.rich)),type="n",xlab="Environmental Bin",
+     ylab="", main="Disturbance",cex.lab=1.5)
 #recent gradient
 sapply(dist.tr$sim.id, function(x)
   points(11 - latgrad.dist.tr[latgrad.dist.tr$sim==x,'region'], 
          latgrad.dist.tr[latgrad.dist.tr$sim==x,'spp.rich'], 
-         type = 'l', lwd=2, col = 'gray80'))
+         type = 'l', lwd=2, col = 'red'))
 sapply(dist.te$sim.id, function(x)
   points(11 - latgrad.dist.te[latgrad.dist.te$sim==x,'region'], 
          latgrad.dist.te[latgrad.dist.te$sim==x,'spp.rich'], 
-         type = 'l', lwd=2, col = 'pink'))
+         type = 'l', lwd=2, col = 'blue'))
 
 #old gradient
 sapply(dist.tr$sim.id, function(x)
   points(11 - oldgrad.dist.tr[oldgrad.dist.tr$sim==x,'region'], 
          oldgrad.dist.tr[oldgrad.dist.tr$sim==x,'spp.rich'], 
-         type = 'l', lwd=2, col = 'gray40', lty='dashed'))
+         type = 'l', lwd=2, col = 'pink', lty='dashed'))
 sapply(dist.te$sim.id, function(x)
   points(11 - oldgrad.dist.te[oldgrad.dist.te$sim==x,'region'], 
          oldgrad.dist.te[oldgrad.dist.te$sim==x,'spp.rich'], 
-         type = 'l', lwd=2, col = 'indianred4', lty='dashed'))
+         type = 'l', lwd=2, col = 'light blue', lty='dashed'))
 
 ## K gradient scenario
-plot(c(1,10),c(0,max(latgrad.Kgrad.tr$spp.rich)),type="n",xlab="Latitude",
-     ylab="Species richness", main="Energetic constraints")
+plot(c(1,10),c(0,max(latgrad.Kgrad.tr$spp.rich)),type="n",xlab="Environmental Bin",
+     ylab="", main="Energetic constraints",cex.lab=1.5)
 #recent gradient
 sapply(Kgrad.tr$sim.id, function(x)
   points(11 - latgrad.Kgrad.tr[latgrad.Kgrad.tr$sim==x,'region'], 
          latgrad.Kgrad.tr[latgrad.Kgrad.tr$sim==x,'spp.rich'], 
-         type = 'l', lwd=2, col = 'gray80'))
+         type = 'l', lwd=2, col = 'red'))
 sapply(Kgrad.te$sim.id, function(x)
   points(11 - latgrad.Kgrad.te[latgrad.Kgrad.te$sim==x,'region'], 
          latgrad.Kgrad.te[latgrad.Kgrad.te$sim==x,'spp.rich'], 
-         type = 'l', lwd=2, col = 'pink'))
+         type = 'l', lwd=2, col = 'blue'))
 
 #old gradient
 sapply(Kgrad.tr$sim.id, function(x)
   points(11 - oldgrad.Kgrad.tr[oldgrad.Kgrad.tr$sim==x,'region'], 
          oldgrad.Kgrad.tr[oldgrad.Kgrad.tr$sim==x,'spp.rich'], 
-         type = 'l', lwd=2, col = 'gray40', lty='dashed'))
+         type = 'l', lwd=2, col = 'pink', lty='dashed'))
 sapply(Kgrad.te$sim.id, function(x)
   points(11 - oldgrad.Kgrad.te[oldgrad.Kgrad.te$sim==x,'region'], 
          oldgrad.Kgrad.te[oldgrad.Kgrad.te$sim==x,'spp.rich'], 
-         type = 'l', lwd=2, col = 'indianred4', lty='dashed'))
+         type = 'l', lwd=2, col = 'light blue', lty='dashed'))
 
 #legend
-legend("topright",c('temperate origin','tropical origin','early','late'), lwd = 2,
-       col = c('pink','gray80','gray40','gray40'), lty=c('solid','solid','dashed','solid'))
+legend("topright",c('bin 10 origin, late','bin 1 origin, late','bin 10 origin, early','bin 1 origin, early'), lwd = 1.5,
+       col = c('blue','red','light blue','pink'), lty=c('solid','solid','dashed','dashed'),cex=0.9)
 dev.off()
