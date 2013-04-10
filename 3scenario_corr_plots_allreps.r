e5b1ad9d4598beaabcaa1aa2b40641d57d4e2f5e
ahhurlbert <hurlbert@bio.unc.edu>
2013-04-10 14:57:39 -0400

    labels and font sizes

--- 3scenario_corr_plots_allreps.r
+++ 3scenario_corr_plots_allreps.r
@@ -29,26 +29,29 @@ Ktemp = read.csv(paste(sim_dir,'/SENC_Stats_K.sims.temp.csv',sep=''), header=T)
 
 
 pdf(paste(analysis_dir,'/summaryplots/corr_cladeorigin_plots_',Sys.Date(),'.pdf',sep=''), height=6, width = 8)
-par(mfrow=c(2,3), mar = c(2,5,2,1), oma=c(4,1,1,1))
+par(mfrow=c(2,3), mar = c(2,2,2,1), oma=c(4,5,1,1))
 cexpts = 0.5
+cexlab = 1.5
+cexmain = 2
 #environment-richness plots
 plot(log10(Ttrop$clade.origin.time), Ttrop$r.env.rich, pch=16, col='red',ylim=c(-1,1),xlab="",
-     ylab=expression(italic(r)[environment-richness]),cex=cexpts, main='Time', cex.lab=2)
+     ylab= "",cex=cexpts, main='Time', cex.lab=2, cex.main=cexmain)
 points(log10(Ttemp$clade.origin.time), Ttemp$r.env.rich, col='blue',cex=cexpts)
+mtext(expression(italic(r)[environment-richness]), 2, at = 0.75, outer=T, line=2, cex = cexlab)
 
 plot(log10(Dtrop$clade.origin.time), Dtrop$r.env.rich, pch=16, col='red',ylim=c(-1,1),xlab="",
-     ylab="",cex=cexpts, main='Disturbance')
+     ylab="",cex=cexpts, main='Disturbance', cex.main=cexmain)
 points(log10(Dtemp$clade.origin.time), Dtemp$r.env.rich, col='blue',cex=cexpts)
 
 plot(log10(Ktrop$clade.origin.time), Ktrop$r.env.rich, pch=16, col='red',ylim=c(-1,1),xlab="",
-     ylab="",cex=cexpts, main='Energy Gradient')
+     ylab="",cex=cexpts, main='Energy Gradient', cex.main=cexmain)
 points(log10(Ktemp$clade.origin.time), Ktemp$r.env.rich, col='blue',cex=cexpts)
 
 # time-richness plots
 plot(log10(Ttrop$clade.origin.time), Ttrop$r.time.rich, pch=16, col='red',ylim=c(-1,1),xlab="",
-     ylab=expression(italic(r)[time-richness]),cex=cexpts, cex.lab=2)
+     ylab= "",cex=cexpts, cex.lab=2)
 points(log10(Ttemp$clade.origin.time), Ttemp$r.time.rich, col='blue',cex=cexpts)
-
+mtext(expression(italic(r)[time-richness]), 2, at = 0.25, outer=T, line=2, cex = cexlab)
 legend('bottomleft',c('temperate origin','tropical origin'), pch = c(1,16), col = c('blue','red'), cex=1)
 
 plot(log10(Dtrop$clade.origin.time), Dtrop$r.time.rich, pch=16, col='red',ylim=c(-1,1),xlab="",
@@ -59,5 +62,5 @@ plot(log10(Ktrop$clade.origin.time), Ktrop$r.time.rich, pch=16, col='red',ylim=c
      ylab="",cex=cexpts)
 points(log10(Ktemp$clade.origin.time), Ktemp$r.time.rich, col='blue',cex=cexpts)
 
-mtext(expression(paste(plain(log)[10]," Time of Clade Origin")),1,outer=T, line=2, cex =1)
+mtext(expression(paste(plain(log)[10]," Time of Clade Origin")),1,outer=T, line=2, cex = cexlab)
 dev.off()
