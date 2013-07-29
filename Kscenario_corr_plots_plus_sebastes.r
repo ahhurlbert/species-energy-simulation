a34b546cf6612b984768b06114d39f61c0e11cf5
ahhurlbert <hurlbert@bio.unc.edu>
2013-07-29 09:57:49 -0400

    added a 4th panel (and rearranged) with latitudinal gradients of two example subclades and the opportunity gradients each faced at their times of origin and expansion

--- Kscenario_corr_plots_plus_sebastes.r
+++ Kscenario_corr_plots_plus_sebastes.r
@@ -8,6 +8,7 @@
 require(ape)
 require(caper)
 
+
 Allen = 1;
 
 if (Allen ==1) {
@@ -57,15 +58,23 @@ for (c in (NEPphy$Nnode+2):max(NEPphy$edge)) {
 lat.corr.output = data.frame(lat.corr.output)
 names(lat.corr.output) = c('cladeID','clade.richness','r.lat.rich','r.lat.rich.gte34','r.lat.rich.lt34')  
 
+# Example clade richness vs opportunity patterns
+source('opportunity_gradient.r')
+sim.for.plot = 3325
+stats = read.csv(paste('C:/SENCoutput/SENC_stats_sim',sim.for.plot,'.csv',sep=''), header=T)
 
+c3157 = reg.opp.grad(cladeid = 3157, sim = sim.for.plot, plot=F, output=T)
+c2486 = reg.opp.grad(cladeid = 2486, sim = sim.for.plot, plot=F, output=T)
+col.3157 = 'gray80' #colors()[577]
+col.2486 = 'black' #colors()[149]
 
 #Plot
-pdf(paste(analysis_dir,'/summaryplots/latcorr_subclades_vs_cladeAgeRich_',Sys.Date(),'.pdf',sep=''), height=15, width = 7)
-par(mfrow=c(3,1), mar = c(6, 3, 2, 1), oma=c(1, 10, 1, 1), mgp = c(5, 1.5, 0))
+pdf(paste(analysis_dir,'/summaryplots/latcorr_subclades_vs_cladeAgeRich_',Sys.Date(),'.pdf',sep=''), height=10, width = 14)
+par(mfcol=c(2,2), mar = c(7, 8, 3, 4), oma=c(1, 1, 1, 4), mgp = c(5.5, 1.5, 0))
 cexpts = 2
 cexpts.seb = 3
 cexaxis = 2
-cexlab = 3
+cexlab = 2.75
 cexleg = 2
 cexabc = 2.5
 pch.temp = 18
@@ -74,40 +83,73 @@ pch.temp = 18
 # plotting complement of x-axis (hence, difference from max value)
 plot(max(log10(Ktrop$clade.origin.time)) - log10(Ktrop$clade.origin.time), -Ktrop$r.env.rich, xaxt = "n",
      xlab = expression(paste(plain(log)[10]," Clade origin time")), pch=16, col='red',ylim=c(-1,1),
-     ylab = "", cex = cexpts, main = '', cex.lab = cexlab, cex.axis = cexaxis, las=1)
+     ylab = expression(italic(r)[latitude-richness]), cex = cexpts, main = '', cex.lab = cexlab, cex.axis = cexaxis, las=1)
 points(max(log10(Ktrop$clade.origin.time)) - log10(Ktemp$clade.origin.time), -Ktemp$r.env.rich, 
            col = 'blue', pch = pch.temp, cex = cexpts)
+points(max(log10(Ktrop$clade.origin.time)) - log10(stats$clade.origin.time[stats$clade.id == 3157]),
+       -stats$r.env.rich[stats$clade.id == 3157], pch = 17, col = col.3157, cex = 1.5*cexpts)
+points(max(log10(Ktrop$clade.origin.time)) - log10(stats$clade.origin.time[stats$clade.id == 2486]),
+       -stats$r.env.rich[stats$clade.id == 2486], pch = 17, col = col.2486, cex = 1.5*cexpts)
 axis(1, at = 0:4, labels = 4:0, cex.axis = cexaxis)
-mtext(c("recent","old"), 1, at = c(0,4), line = 4, cex = 1.5)
+mtext(c("recent","old"), 1, at = c(0,4), line = 3, cex = 1.5)
 abline(h = 0, lty = 'dashed')
 legend('topright',c('temperate origin','tropical origin'), pch = c(pch.temp, 16), col = c('blue','red'), cex = cexleg)
-mtext("(a)", 2, at = 1, cex = cexabc, outer = T, las = 1, line = 5)
+mtext("(a)", 2, at = 1.3, cex = cexabc, outer = F, las = 1, line = 5)
 
 #Vs clade richness
 plot(log10(Ktrop$clade.richness), -Ktrop$r.env.rich, pch = 16, col = 'red', ylim = c(-1,1),
-     xlab = expression(paste(plain(log)[10]," Clade richness")), ylab = "",
+     xlab = expression(paste(plain(log)[10]," Clade richness")), ylab = expression(italic(r)[latitude-richness]),
      cex = cexpts, main = '', cex.lab = cexlab, cex.axis = cexaxis, las = 1)
 points(log10(Ktemp$clade.richness), -Ktemp$r.env.rich, col = 'blue', pch = pch.temp, cex = cexpts)
+points(log10(stats$clade.richness[stats$clade.id == 3157]),
+       -stats$r.env.rich[stats$clade.id == 3157], pch = 17, col = col.3157, cex = 2*cexpts)
+points(log10(stats$clade.richness[stats$clade.id == 2486]),
+       -stats$r.env.rich[stats$clade.id == 2486], pch = 17, col = col.2486, cex = 2*cexpts)
 abline(h = 0,lty = 'dashed')
-mtext("Latitude-richness correlation", 2, outer=T, cex = cexaxis, line = 3)
-mtext("(b)", 2, outer=T, at = .67, cex = cexabc, las = 1, line = 5)
+mtext("(b)", 2, outer=F, at = 1.3, cex = cexabc, las = 1, line = 5)
 
-#extra tick marks showing % of max richness
-#pcts = c(.9,.3,.1,.03,.01)
-#par(mgp=c(-3,0,0))
-#axis(1,at=log10(pcts*max(Ktrop$clade.richness)), labels=F,tck= .01)
-#text(log10(pcts*max(Ktrop$clade.richness)), rep(-1,length(pcts)), paste(pcts*100,"%",sep=""))
+
+#Plot example richness and opportunity gradients for two examples
+lat.bin = 11 - c3157$region
+eq.rich.grad = lm(c3157$spp.rich ~ lat.bin)
+
+plot(11 - c3157$region,c3157$spp.rich, type='n', xlim=c(1,10), ylim = c(0,500),
+     xlab = 'Latitude', ylab = 'Species richness', xaxt = "n", cex.lab = cexlab, cex.axis = cexaxis, las=1)
+lines(10:1, predict(eq.rich.grad, newdata = data.frame(1:10)), lwd = 2)
+points(11 - c2486$region, c2486$clade.rich, type = 'l', col = col.2486, lwd = 5)
+arrows(11 - c2486$reg.of.origin, -0.25*max(c2486$spp.rich), 11 - c2486$reg.of.origin, -20, col = col.2486, lwd = 3, xpd = T)
+points(11 - c3157$region, c3157$clade.rich, type = 'l', col = col.3157, lwd = 5)
+arrows(11 - c3157$reg.of.origin, -0.25*max(c3157$spp.rich), 11 - c3157$reg.of.origin, -20, col = col.3157, lwd = 3, xpd = T)
+mtext("(c)", 2, at = 570, outer = F, cex = cexabc, las = 1, line = 5)
+par(new=T)
+ymax = 1.2
+plot(11-c3157$region, c3157$opp.frac, type = 'l', xlim = c(1,10), col=col.3157, yaxt="n", xaxt = "n",
+     xlab="", ylab="", ylim=c(0, ymax), lwd = 5, lty = 'dashed')
+points(11-c2486$region, c2486$opp.frac, type = 'l', xlim = c(1,10), col=col.2486, 
+     ylim=c(0, ymax), lwd = 5, lty = 'dashed')
+axis(4, at = seq(0, 1, by = .2), cex.axis = cexaxis, las = 1)
+axis(1, at = 1:10, labels = F)
+mtext("Opportunity", 4, outer=T, cex = 2.25, adj = 0.84, line = 2)
+mtext("Tropics",1,adj=.05,line=1.5, cex=1.5)
+mtext("Temperate",1,adj=.95,line=1.5, cex=1.5)
+
 
 #Sebastes vs clade richness
 plot(log10(lat.corr.output$clade.richness), lat.corr.output$r.lat.rich, ylim = c(-1,1), las=1, cex.axis = cexaxis, cex.lab = cexlab,
-     xlab = expression(paste(plain(log)[10]," Clade richness")), ylab = 'Latitude-richness correlation', pch = 16, cex = cexpts.seb)
+     xlab = expression(paste(plain(log)[10]," Clade richness")), ylab = expression(italic(r)[latitude-richness]), pch = 16, cex = cexpts.seb)
 points(log10(lat.corr.output$clade.richness), lat.corr.output$r.lat.rich.gte34, pch = 1, cex = cexpts.seb)
 #points(log10(lat.corr.output$clade.richness), lat.corr.output$r.lat.rich.lt34, pch=2, cex = cexpts)
 abline(h=0,lty='dashed')
 legend("topright", c('Entire gradient','North of 34N'), pch = c(16, 1), cex = cexleg)
-mtext("(c)", 2, at = 0.33, outer = T, cex = cexabc, las = 1, line = 5)
-
+mtext("(d)", 2, outer = F, at = 1.3, cex = cexabc, las = 1, line = 5)
 dev.off()
+
+
+
+
+
+
+
 
 ###############
 # Clade richness vs time of clade origin, to help interpret above patterns
