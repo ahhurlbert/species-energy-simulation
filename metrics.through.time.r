3ee6e55c61f39d3a5b8c81746e9778e2c66458fb
ahhurlbert <hurlbert@bio.unc.edu>
2013-09-10 11:28:41 -0400

    modified so that latitude-richness correlation is plotted in panel (b) rather than environment-richness correlation

--- metrics.through.time.r
+++ metrics.through.time.r
@@ -26,18 +26,20 @@ temp.sims = 4075:4077
 
 sim.matrix = read.csv("SENC_Master_Simulation_Matrix.csv",header=T);
 
-trop.metrics = array(-999,c(length(trop.sims),100,27));
-temp.metrics = array(-999,c(length(temp.sims),100,27));
+trop.metrics = array(-999,c(length(trop.sims),100,28));
+temp.metrics = array(-999,c(length(temp.sims),100,28));
 
 #Need same number of sims in trop.sims and temp.sims
 for (i in 1:length(trop.sims)) {
   
   sim1 = trop.sims[i];
   sim2 = temp.sims[i]
-  temp1 = read.csv(paste(sim_dir,"/SENC_Stats_sim",sim1,"_mult_times.csv",sep=""),header=T);
-  trop.metrics[i,,] = as.matrix(temp1);
+  temp1 = read.csv(paste(sim_dir,"/SENC_Stats_sim",sim1,"_mult_times.csv",sep=""),header=T)
+  temp1$r.lat.rich = -temp1$r.env.rich; 
+  trop.metrics[i,,] = as.matrix(temp1); 
   trop.metrics = array(trop.metrics, dim = dim(trop.metrics), dimnames = list(NULL, NULL, colnames(temp1)));
   temp2 = read.csv(paste(sim_dir,"/SENC_Stats_sim",sim2,"_mult_times.csv",sep=""),header=T);
+  temp2$r.lat.rich = -temp2$r.env.rich;
   temp.metrics[i,,] = as.matrix(temp2);
   temp.metrics = array(temp.metrics, dim = dim(temp.metrics), dimnames = list(NULL, NULL, colnames(temp2)));
   rm(list = c('temp1', 'temp2'));
@@ -53,10 +55,10 @@ trop.metrics.sd = data.frame(apply(trop.metrics, 2:3, function(x) var(x)^.5))
 pdf(paste(analysis_dir,'/metrics_thru_time_',Sys.Date(),'.pdf',sep=""), height = 6, width = 8)
 par(mfrow = c(2, 2), mar = c(3, 6, 1, 1), oma = c(5, 0, 0, 0), cex.lab = 2, las = 1, cex.axis = 1.3, mgp = c(4,1,0))
 y.labels = c('Global richness', expression(italic(r)[latitude-richness]), expression(gamma), expression(italic(r)[MRD-richness]))
-metric.names = c('global.richness','r.env.rich', 'gamma.stat','r.MRD.rich')
+metric.names = c('global.richness','r.lat.rich', 'gamma.stat','r.MRD.rich')
 for (j in 1:4) {
   curr.metric = metric.names[j]
-  plot(trop.metrics[1, , 'time']/1000, metrics[1, , curr.metric], xlim = c(0, max(trop.metrics[,,'time']/1000)), 
+  plot(trop.metrics.mean$time/1000, trop.metrics.mean[, curr.metric], xlim = c(0, max(trop.metrics.mean$time/1000)), 
        ylim = range(c(trop.metrics[, , curr.metric], temp.metrics[, , curr.metric])), type = "n",
        ylab = y.labels[j], xlab = "")
   polygon(c(trop.metrics.mean$time/1000, rev(trop.metrics.mean$time/1000)), 
