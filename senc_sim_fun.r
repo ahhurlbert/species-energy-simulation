2fcac4ef28eb1cb5d65a477060d0d5bed19f8bb6
ahhurlbert <hurlbert@bio.unc.edu>
2013-10-01 12:21:55 -0400

    cleaned up formatting (including deleting uses of 'else{}' and 'which') up through the time loop; uncertain about why we are writing to a file with extinct populations in the second to last if statement in this loop; do we need that?

--- senc_sim_fun.r
+++ senc_sim_fun.r
@@ -141,137 +141,151 @@ senc_sim_fun = function(sim.matrix, sim) {
 
 
 	# Start main time loop
-  #---------------------
+  #------------------------------------------------------
 	for (curr.time in 1:max.time) {
 
 		## update population sizes
-		all.populations$env.fit = env.fit.fun(w.param=w,E=all.populations$reg.env,spp.opt=all.populations$env.opt)
-		all.populations$pop.size = all.populations$env.fit*all.populations$carry.cap*all.populations$extant
-		reg.pop.sizes = as.data.frame(tapply(all.populations$pop.size,all.populations$region,FUN=sum))
-		reg.pop.sizes = cbind(rownames(reg.pop.sizes),reg.pop.sizes) colnames(reg.pop.sizes) = c("region","reg.individuals")
-		all.populations = merge(all.populations,reg.pop.sizes,by='region')
-		all.populations$pop.multiplier = all.populations$carry.cap/all.populations$reg.individuals
+		all.populations$env.fit = env.fit.fun(w.param = w,E = all.populations$reg.env, spp.opt = all.populations$env.opt)
+		all.populations$pop.size = all.populations$env.fit * all.populations$carry.cap * all.populations$extant
+		reg.pop.sizes = as.data.frame(tapply(all.populations$pop.size, all.populations$region, FUN = sum))
+		reg.pop.sizes = cbind(rownames(reg.pop.sizes), reg.pop.sizes) 
+    colnames(reg.pop.sizes) = c("region", "reg.individuals")
+		all.populations = merge(all.populations, reg.pop.sizes, by = "region")
+		
+    #The pop.multiplier assures that the sum of species abundances will not exceed carrying capacity
+    all.populations$pop.multiplier = all.populations$carry.cap / all.populations$reg.individuals
 		all.populations$pop.multiplier[all.populations$pop.multiplier > 1] = 1
-		if (carry.cap == 'off') {all.populations$pop.multiplier = 1} else{} # if there is no carrying capacity, then population size is not influenced by the actual community-level number of individuals
-		all.populations$pop.size = round( all.populations$pop.size * all.populations$pop.multiplier ,digits=0)
-		reg.pop.sizes = as.data.frame(tapply(all.populations$pop.size,all.populations$region,FUN=sum))
-		reg.pop.sizes = cbind(rownames(reg.pop.sizes),reg.pop.sizes) colnames(reg.pop.sizes) = c("region","reg.individuals.real")
-		all.populations = merge(all.populations,reg.pop.sizes,by='region')
     
-    if (is.element(curr.time,disturb_times)==T) {
-      
+		# if there is no carrying capacity, then population size is not influenced by the actual 
+    # community-level number of individuals, and so the sum across species is not modified (pop.multiplier = 1)
+    if (carry.cap == 'off') {all.populations$pop.multiplier = 1} 
+		
+    all.populations$pop.size = round( all.populations$pop.size * all.populations$pop.multiplier , digits = 0)
+		reg.pop.sizes = as.data.frame(tapply(all.populations$pop.size, all.populations$region, FUN = sum))
+		reg.pop.sizes = cbind(rownames(reg.pop.sizes), reg.pop.sizes) 
+    colnames(reg.pop.sizes) = c("region", "reg.individuals.real")
+		all.populations = merge(all.populations, reg.pop.sizes, by = 'region')
+    
+    # Perturb communities if disturbance has been specified and it's 'time'
+    if (is.element(curr.time, disturb_times) == T) {
       for (reg.sampled in unique(all.populations$region[all.populations$extant==1])) {
-        
         reg.disturb = reg.disturb.intensity$intensity[reg.disturb.intensity$region==reg.sampled]
-        all.populations$pop.size[all.populations$region==reg.sampled & all.populations$extant==1] = round((1-reg.disturb)*all.populations$pop.size[all.populations$region==reg.sampled & all.populations$extant==1],digits=0)
-        
+        all.populations$pop.size[all.populations$region == reg.sampled & all.populations$extant==1] 
+          = round((1 - reg.disturb) * all.populations$pop.size[all.populations$region == reg.sampled & 
+                                                                 all.populations$extant == 1], digits = 0)
       }  
-          
     }
 
-		## evaluate mutation across all extant species
-		num.of.mutations = mutation.fun(population.size=all.populations$pop.size,alpha.param=alpha)
+		# evaluate mutation across all extant species
+		num.of.mutations = mutation.fun(population.size = all.populations$pop.size, alpha.param = alpha)
 		all.populations$mutate = num.of.mutations
 
-		## find new trait values for mutants
-		all.populations$mutant.env.opt = all.populations$env.opt + mut.opt.fun(sigma_E,min.uni,max.uni,num.to.generate=nrow(all.populations))
+		# find new trait values for mutants
+		all.populations$mutant.env.opt = all.populations$env.opt + mut.opt.fun(sigma_E, num.to.generate = nrow(all.populations))
 
-		## evaluate dispersal across all extant species
-		num.of.dispersals = dispersal.fun(population.size=all.populations$pop.size,beta.param=beta)
-		all.populations$disperse = num.of.dispersals
+		# evaluate dispersal across all extant species
+		all.populations$disperse = dispersal.fun(population.size = all.populations$pop.size, beta.param = beta)
+		
+		# evaluate extinction across all extant species
+		extinction.probs = extinction.fun(gamma.param = gamma, population.size = all.populations$pop.size)
+		all.populations$extinction = apply(as.matrix(extinction.probs), 1, function(p) rbinom(1, 1, prob = p))
 
-		## evaluate extinction across all extant species
-		extinction.probs = extinction.fun(gamma.param=gamma,population.size=all.populations$pop.size)
-		all.populations$extinction = apply(as.matrix(extinction.probs), 1, function(p) rbinom(1, 1, prob=p))
-
-		# collecting richness data, including only those species that have persisted across at least one time step, so not including new arrival via dispersal or mutation
+		# collecting richness data, including only those species that have persisted across at least one time step, 
+    # so not including new arrival via dispersal or mutation
 		for (i in 0:num.of.bins) {
-
-			time.richness[time.rich.row.id,] = c(curr.time,i,length(all.populations$region[all.populations$region==i & all.populations$extinction==0]),reg.E.K.$reg.env[reg.E.K.$region==i])
+			time.richness[time.rich.row.id,] = c(curr.time, i, length(all.populations$region[all.populations$region == i & all.populations$extinction==0]),
+                                           reg.E.K.$reg.env[reg.E.K.$region==i])
 			time.rich.row.id = time.rich.row.id + 1
-
 		}
 
-		## build attributes used in phylogeny construction
-		mutated.populations = all.populations[all.populations$mutate>0,]
-		# leave only one mutant from each species. Assuming there are at most two mutations per species, choose the region of mutation randomly...see the 'sample' piece of the code
-		mutated.populations = mutated.populations[is.element(rownames(mutated.populations),rownames(unique(mutated.populations[,c('spp.name','env.opt')],fromLast = sample(c(FALSE,TRUE),1))))==TRUE,]
-
-		if (nrow(mutated.populations)>0) {total.mutations = total.mutations + 1} else{}
-
-		if (nrow(mutated.populations)>0 & total.mutations == 1) { # this is just for the first mutation event
-	
-			stem.depth = as.numeric(c(curr.time,curr.time))
-			all.populations = rbind(all.populations,c(mutated.populations$region,next.spp,1,mutated.populations$mutant.env.opt,curr.time,max.time+1,curr.time,max.time+1,rep(-999,(ncol(all.populations)-8))))
-
-		} else{}
-
-		if (nrow(mutated.populations)>0 & total.mutations > 1) { # this handles all mutations after the first mutation
-
+		# build attributes used in phylogeny construction
+		mutated.populations = all.populations[all.populations$mutate > 0,]
+		# leave only one mutant from each species. Assuming there are at most two mutations per species, 
+    # choose the region of mutation randomly...see the 'sample' piece of the code
+		mutated.populations = mutated.populations[is.element(rownames(mutated.populations), 
+                                                         rownames(unique(mutated.populations[,c('spp.name','env.opt')],
+                                                                         fromLast = sample(c(FALSE,TRUE),1))))==TRUE,]
+		# this is just for the first mutation event
+		if (nrow(mutated.populations) > 0 & total.mutations == 1) { 
+      total.mutations = total.mutations + 1	
+			stem.depth = as.numeric(c(curr.time, curr.time))
+			all.populations = rbind(all.populations, 
+                              c(mutated.populations$region, next.spp, 1, mutated.populations$mutant.env.opt,
+                                curr.time, max.time + 1, curr.time, max.time + 1, rep(-999, ncol(all.populations) - 8)))
+		}
+		
+    # this handles all mutations after the first mutation
+		if (nrow(mutated.populations) > 0 & total.mutations > 1) { 
+		  total.mutations = total.mutations + 1
 			for (i in 1:nrow(mutated.populations)) {
 
 				next.spp = next.spp + 1
 				mutated.sp = mutated.populations$spp.name[i] # this is the true species name, not a node name, so it doesn't change.
-				mutated.node = edge$to.node[edge$spp==mutated.sp]
-				e = edge[which(edge$alive==1), ]
-				edge$alive[which(edge$to.node==mutated.node)] = 0 
-				edge$spp[edge$spp==mutated.sp] = -999
-				edge = rbind(edge, c(mutated.node, next.node,1,mutated.sp), c(mutated.node, next.node + 1,1,next.spp))
+				mutated.node = edge$to.node[edge$spp == mutated.sp]
+				e = edge[which(edge$alive == 1), ]
+				edge$alive[which(edge$to.node == mutated.node)] = 0 
+				edge$spp[edge$spp == mutated.sp] = -999
+				edge = rbind(edge, c(mutated.node, next.node, 1, mutated.sp), c(mutated.node, next.node + 1, 1, next.spp))
 				next.node = next.node + 2
 
-				stem.depth <- c((stem.depth), curr.time, curr.time)
+				stem.depth <- c(stem.depth, curr.time, curr.time)
 				x <- which(edge[, 2] == mutated.node)
 				edge.length[x] <- curr.time - stem.depth[x]
 				edge.length <- c(edge.length, NA, NA)
 
-				all.populations = rbind(all.populations,c(mutated.populations$region[i],next.spp,1,mutated.populations$mutant.env.opt[i],curr.time,max.time+1,curr.time,max.time+1,rep(-999,(ncol(all.populations)-8))))
-
+				all.populations = rbind(all.populations,
+                                c(mutated.populations$region[i], next.spp, 1, mutated.populations$mutant.env.opt[i],
+                                  curr.time, max.time + 1, curr.time, max.time + 1, rep(-999, ncol(all.populations)-8)))
 			}
+		} 
 
-		} else{}
-
-		## disperse species that were selected to do so	
-		disp.spp = all.populations[all.populations$disperse>0,]
-		if ( nrow(disp.spp)>0 ) {
-			disp.spp$region = disp.spp$region + sample(c(-1,1),length(disp.spp$region),replace=T,prob=c(0.5,0.5))
+		# disperse species that were selected to do so	
+		disp.spp = all.populations[all.populations$disperse > 0, ]
+		if ( nrow(disp.spp) > 0 ) {
+			disp.spp$region = disp.spp$region + sample(c(-1,1), length(disp.spp$region), replace=T, prob = c(0.5,0.5))
 			disp.spp$time.of.origin = curr.time
-			disp.spp = disp.spp[which(is.element(apply(disp.spp[,1:4],1 ,paste,collapse=","),apply(all.populations[,1:4],1 ,paste,collapse=","))==F),]
-			all.populations = rbind(all.populations,disp.spp)
-			all.populations = all.populations[is.element(all.populations$region,seq(0,num.of.bins,1))==TRUE,] # take out any populations that dispersed beyond the bins being evaluated
-		} else{}
+			disp.spp = disp.spp[is.element(apply(disp.spp[, 1:4], 1, paste, collapse=","),
+                                     apply(all.populations[, 1:4], 1, paste, collapse=",")) == F, ]
+			all.populations = rbind(all.populations, disp.spp)
+			# take out any populations that dispersed beyond the bins being evaluated
+			all.populations = all.populations[is.element(all.populations$region, 0:num.of.bins) == TRUE, ] 
+		}
 
-		## remove those species that are going extinct
-		all.populations$time.of.extinction[all.populations$extinction==1 & all.populations$extant==1] = curr.time 
-		extinct.this.time = unique(all.populations$spp.name[all.populations$extinction==1 & all.populations$extant==1]) # a vector of species with populations going extinct this time step
-		all.populations$extant[all.populations$extinction==1 & all.populations$extant==1] = 0
+		# remove those species that are going extinct
+		all.populations$time.of.extinction[all.populations$extinction == 1 & all.populations$extant == 1] = curr.time 
+		# --create a vector of species with populations going extinct this time step
+		extinct.this.time = unique(all.populations$spp.name[all.populations$extinction == 1 & all.populations$extant == 1]) 
+		all.populations$extant[all.populations$extinction == 1 & all.populations$extant == 1] = 0
 
-		## find those species that went globally extinct this time step
-		extant.spp = unique(all.populations$spp.name[all.populations$extant==1])
-		global.extinct.this.step = extinct.this.time[is.element(extinct.this.time,extant.spp)==F]
-		all.populations$time.of.sp.extinction[which(is.element(all.populations$spp.name,global.extinct.this.step)==T)] = curr.time
+		# find those species that went globally extinct this time step
+		extant.spp = unique(all.populations$spp.name[all.populations$extant == 1])
+		global.extinct.this.step = extinct.this.time[is.element(extinct.this.time, extant.spp) == F]
+		all.populations$time.of.sp.extinction[is.element(all.populations$spp.name, global.extinct.this.step) == T] = curr.time
 
-		## change the 'alive' status for those species that went globally extinct this time step
-		edge$alive[is.element(edge$spp,global.extinct.this.step)==T] = 0
-		edge.length[which(is.element(edge$spp,global.extinct.this.step)==T)] = curr.time - stem.depth[which(is.element(edge$spp,global.extinct.this.step)==T)]
+		# change the 'alive' status for those species that went globally extinct this time step
+		edge$alive[is.element(edge$spp, global.extinct.this.step) == T] = 0
+		edge.length[is.element(edge$spp,global.extinct.this.step) == T] = curr.time - 
+                                                            stem.depth[is.element(edge$spp, global.extinct.this.step) == T]
 
-		## update columns for carry.cap, reg.env, and reg.richness
-		all.populations = all.populations[,1:8]
-		all.populations = merge(all.populations,reg.E.K.,by='region')
+		# update columns for carry.cap, reg.env, and reg.richness
+		all.populations = all.populations[ , 1:8]
+		all.populations = merge(all.populations, reg.E.K., by='region')
 
-		tot.richness = length(unique(all.populations$spp.name[all.populations$extant==1]))
+		tot.richness = length(unique(all.populations$spp.name[all.populations$extant == 1]))
 
 		#if (is.element(curr.time,print.times)==T) {print(c(curr.time,nrow(all.populations),date(),tot.richness))} else{}
 
-		if (length(all.populations$extant[all.populations$extant==0]) > 500) {
+		if (length(all.populations$extant[all.populations$extant == 0]) > 500) {
 
-			extinct.pops = subset(all.populations,extant==0)
+			extinct.pops = subset(all.populations, extant == 0)
 			tot.extinct.pops = tot.extinct.pops + nrow(extinct.pops)
-			write.csv(extinct.pops,paste("temp.extinct.sim.",sim,".time.",curr.time,".csv",sep=""),row.names=F,quote=F)
-			all.populations = subset(all.populations,extant==1)
-			extinct.pops.output.times = c(extinct.pops.output.times,curr.time)
+			write.csv(extinct.pops, paste("temp.extinct.sim.", sim, ".time.", curr.time, ".csv", sep=""), row.names=F, quote=F)
+			all.populations = subset(all.populations, extant == 1)
+			extinct.pops.output.times = c(extinct.pops.output.times, curr.time)
 
-		} else{}
+		} 
 
+    # Stop simulation if total richness has exceeded the specified threshold
 		if (tot.richness >= max.richness) {break} else{}
 
 	} 
