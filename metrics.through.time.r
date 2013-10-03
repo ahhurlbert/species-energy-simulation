a340cc6918aa5c1ebe7fc7d60d196b0199b82c7e
ahhurlbert <hurlbert@bio.unc.edu>
2013-10-03 09:25:12 -0400

    deleted obsolete metric.abind function, as well as exploratory histogram plots at the end

--- metrics.through.time.r
+++ metrics.through.time.r
@@ -8,7 +8,7 @@ Allen = 0;
 #New parameter for taking into account which of us is running this code
 if(Allen==1) {
   setwd('c:/documents and settings/hurlbert/species-energy-simulation')
-  sim_dir = "C:/SENCoutput/longtime_reps"
+  sim_dir = "C:/SENCoutput"
   analysis_dir = "//bioark.bio.unc.edu/hurlbertallen/manuscripts/cladevscommunity/analyses/summaryplots"
 } else {
   setwd('C:/Users/steg815/Desktop/Stegen_PNNL/Spp-Energy-Niche-Conserv/species-energy-simulation')
@@ -24,7 +24,7 @@ temp.sims = c(4075:4084, 4275:4364)
 #No energy gradient sims
 # currently has 100 sims
 Ttrop.sims = 3465:3564
-Ttemp.sims = c(3565:3646,3648:3664)
+Ttemp.sims = 3565:3664
 
 # next 5 lines are temporary
 #run.sims = list.files(path = "//constance/people/steg815/senc.analysis", pattern='_time_seq_root_only.csv');
@@ -35,35 +35,6 @@ Ttemp.sims = c(3565:3646,3648:3664)
 
 sim.matrix = read.csv("SENC_Master_Simulation_Matrix.csv",header=T);
 
-# The 'metric.abind' function is currently obsolete. Use or modify the 'metric.abind.new' function
-metric.abind = function(sims, scenario = "K", min.n.regions = 4, min.richness = 30) {
-  metrics = matrix(NA, nrow = 100, ncol = 28)
-  for (i in sims) {
-    if (scenario == "K") {
-      temp = read.csv(paste(sim_dir,"/SENC_Stats_sim",i,"_mult_times.csv",sep=""),header=T)
-    } else if (scenario == "T") {
-      temp = read.csv(paste(sim_dir,"/SENC_Stats_sim",i,"_time_seq_root_only.csv",sep=""),header=T)
-    }
-    temp$r.lat.rich = -temp$r.env.rich
-    # There is no output for timesteps in which no correlations could be calculated
-    # so we add the relevant number of rows of data with NA's in that case
-    if (nrow(temp) < 100) {
-      temp.top = data.frame(matrix(NA, nrow = 100 - nrow(temp), ncol = 28))
-      names(temp.top) = names(temp)
-      temp = rbind(temp.top, temp)
-    }
-    # Only include metrics in analysis if simulation meets minimum region and richness criteria
-    if (min(temp$n.regions, na.rm = T) < min.n.regions) {
-      temp[which(temp$n.regions < min.n.regions),] = NA
-    }
-    if (min(temp$global.richness, na.rm = T) < min.richness) {
-      temp[which(temp$global.richness < min.richness),] = NA
-    }
-    metrics = abind(metrics, temp, along = 3)
-  }
-  return(metrics[,,-1]) #don't include the first slice of NAs
-}
-
 metric.abind.new = function(sims, scenario = "K", min.div.regions = 4, min.richness = 30) {
   num.cols = 41
   metrics = matrix(NA, nrow = 100, ncol = num.cols)
@@ -71,7 +42,7 @@ metric.abind.new = function(sims, scenario = "K", min.div.regions = 4, min.richn
     #if (scenario == "K") {
     #  temp = read.csv(paste(sim_dir,"/NEW_Stats_sim",i,"_mult_times.csv",sep=""),header=T)
     #} else if (scenario == "T") {
-      temp = read.csv(paste(sim_dir,"/NEW_Stats_sim",i,"_time_seq_root_only.csv",sep=""),header=T)
+      temp = read.csv(paste(sim_dir,"/lbeta_Stats_sim",i,"_time_seq_root_only.csv",sep=""),header=T)
     #}
     temp$r.lat.rich = -temp$r.env.rich
     temp$scaled.MRD.range = temp$MRD.range/temp$max.RD
@@ -168,18 +139,18 @@ metric.labels = c('Global richness',
                   'Mean PSV',
                   expression(beta),
                   'scaled MRD range',
-                  'scaled MRD-richness slope')
+                  'MRD-richness slope')
 
 
 # Plot 4 metrics over the course of the simulation: global richness, the latitude-richness correlation, 
 # gamma, and the MRD-richness correlation. Means +/- 2 SD are shown.
-pdf(paste(analysis_dir,'/metrics_thru_time_inc_Tscenario',Sys.Date(),'_',min.num.regions,'.pdf',sep=""), height = 6, width = 8)
+pdf(paste(analysis_dir,'/metrics_thru_time_inc_Tscenario_',Sys.Date(),'_',min.num.regions,'.pdf',sep=""), height = 6, width = 8)
 par(mfrow = c(2, 2), mar = c(5, 6, 1, 1), oma = c(5, 0, 0, 0), cex.lab = 2, las = 1, cex.axis = 1.3, mgp = c(4,1,0))
 
 # Specify variables to plot here, and width of error bars
-#names4plotting = c('global.richness','r.lat.rich', 'gamma.stat','MRD.rich.slope')
+names4plotting = c('global.richness','r.lat.rich', 'scaled.MRD.rich.slope', 'tree.beta')
 #names4plotting = c('r.env.PSV', 'r.env.MRD', 'r.MRD.rich','r.PSV.rich')
-names4plotting = c('MRD.rich.slope', 'scaled.MRD.rich.slope','scaled.MRD.range','tree.beta')
+#names4plotting = c('MRD.rich.slope', 'scaled.MRD.rich.slope','scaled.MRD.range','tree.beta')
 error = 2 # error bars in SD units (+/-)
 for (j in 1:4) {
   curr.metric = names4plotting[j]
@@ -223,39 +194,3 @@ for (j in 1:4) {
 mtext("Time (x1000, Energy Gradient)", 1, outer=T, cex = 1.75, line = 1.5) 
 mtext("Time (No Energy Gradient)", 1, outer = T, cex = 1.75, line = 3.5, col = 'gray50')
 dev.off()
-
-## Plot histograms of metrics, putting all 4 scenarios on each panel and different panels had different metrics
-
-#par(mfrow = c(2, 2), mar = c(3, 6, 1, 1), oma = c(3, 0, 0, 0), cex.lab = 2, las = 1, cex.axis = 1.3, mgp = c(4,1,0))
-par(mfrow = c(2, 2), cex.lab = 2, cex.axis = 1.3,mar = c(5, 6, 1, 1),oma = c(3, 0, 0, 0),las=1,mgp = c(4,1,0))
-
-metric.names = c('global.richness','r.lat.rich', 'gamma.stat','r.env.PSV', 'r.env.MRD', 'r.MRD.rich','r.PSV.rich')
-metric.labels = c('Global richness', expression(italic(r)[latitude-richness]), 
-                  expression(gamma), expression(italic(r)[env-PSV]),
-                  expression(italic(r)[env-MRD]), expression(italic(r)[MRD-richness]),
-                  expression(italic(r)[PSV-richness]))
-
-
-names4plotting = c('r.env.PSV', 'r.env.MRD', 'r.MRD.rich','r.PSV.rich')
-for (j in 1:4) {
-  curr.metric = names4plotting[j]
-  Ttrop.hist = density(Ttrop.metrics[, curr.metric,],na.rm=T); Ttrop.hist$y = Ttrop.hist$y / max(Ttrop.hist$y,rm.na=T);
-  Ttemp.hist = density(Ttemp.metrics[, curr.metric,],na.rm=T); Ttemp.hist$y = Ttemp.hist$y / max(Ttemp.hist$y,rm.na=T);
-  trop.hist = density(trop.metrics[, curr.metric,],na.rm=T); trop.hist$y = trop.hist$y / max(trop.hist$y,rm.na=T);
-  temp.hist = density(temp.metrics[, curr.metric,],na.rm=T); temp.hist$y = temp.hist$y / max(temp.hist$y,rm.na=T);
-  #max(c(Ttrop.hist$y,Ttemp.hist$y,trop.hist$y,temp.hist$y))
-  
-  plot(Ttrop.hist,xlim=c(-1,1),xlab = metric.labels[metric.names == curr.metric],main="",ylim=c(0,1),typ="n")
-  points(Ttrop.hist,type = 'l', col = 'red', lwd = 3, lty = 'dashed')
-  points(Ttemp.hist,type = 'l', col = 'blue', lwd = 3, lty = 'dashed')
-  points(trop.hist,type = 'l', col = 'red', lwd = 3, lty = 1)
-  points(temp.hist,type = 'l', col = 'blue', lwd = 3, lty = 1)
-  
-}
-
-# just finding sims with odd metric values
-for (i in 1:100) {
-  
-  print(Ttemp.metrics[which.min(Ttemp.metrics[,curr.metric,i]),c('sim',curr.metric),i])
-    
-}
