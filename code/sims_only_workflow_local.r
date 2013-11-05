be30fce3598b5bf5e95af63768928e6954a48ec9
ahhurlbert <hurlbert@bio.unc.edu>
2013-11-05 15:48:58 -0500

    eliminated Allen==1 toggle, reformatted with spaces, eliminated ;s, changed destination of written output to raw_sim_output folder

--- code/sims_only_workflow_local.r
+++ code/sims_only_workflow_local.r
@@ -1,35 +1,28 @@
 # Simulation workflow
-user="Allen"
-partial.analysis=1
+
+#(1) load libraries and simulation and phylogeny-making functions
+library(ape)
+library(permute)
+library(nlme)
+library(vegan)
+library(picante)
+library(mvtnorm)
+library(caper)
+library(paleotree)
+library(plyr)
+library(parallel)
+library(doParallel)
+
+# Info for parallelizing processing specify number of processors in makeCluster
+package.vector = c('ape','permute','nlme','vegan','picante','mvtnorm','caper','paleotree','plyr','phytools')
+cl = makeCluster(2)
+registerDoParallel(cl)
+
+source("code/senc_sim_fun.r")
+source("code/make.phylo.jimmy.fun.r")
 
 
-# (1) set wd for location to write results
-if (user=="Allen") {
-  setwd("C:/SENCoutput")
-}
-if (user=="James") {
-  setwd("C:/Users/steg815/Desktop/Stegen_PNNL/Spp-Energy-Niche-Conserv/sims.out.130204")
-}
-
-
-#(2) load libraries simulation and analysis functions
-library(ape);
-library(permute);
-library(nlme);
-library(vegan);
-library(picante);
-library(mvtnorm);
-library(caper);
-library(paleotree);
-library(plyr);
-library(parallel);
-library(doParallel);
-
-
-source('code/senc_sim_fun.r')
-source('code/make.phylo.jimmy.fun.r')
-
-#(3) read in master simulation matrix with chosen parameter combinations
+#(2) read in master simulation matrix with chosen parameter combinations
 # then add fields for storing output summary
 sim.matrix = read.csv("SENC_Master_Simulation_Matrix.csv", header=T)
 sim.matrix$n.regions = NA
@@ -40,25 +33,22 @@ sim.matrix$skipped.times = NA
 sim.matrix$BK.reg = NA
 sim.matrix$BK.env = NA
 
-package.vector = c('ape','permute','nlme','vegan','picante','mvtnorm','caper','paleotree','plyr','phytools');
-
-cl = makeCluster(2);
-registerDoParallel(cl);
 
 
-#(4) start analyses based on value of 'sim' which draws parameter values from sim.matrix
-if (partial.analysis == 0) {which.sims = 1:max(sim.matrix$sim.id)};
-if (partial.analysis == 1) {which.sims = 3125:3324}; # which.sims = c(read.csv(paste(analysis_dir,"/sims.to.analyze.csv",sep=""))$x)
+#(3) Specify which simulation IDs to run, as given in the sim.matrix
+which.sims = 3125:3324
 
-foo = foreach(sim=which.sims,.packages = package.vector,.combine='rbind') %dopar% {
+foo = foreach(sim = which.sims, .packages = package.vector, .combine = "rbind") %dopar% {
   
-  #(4) call simulation
-  sim.start = date(); print(sim.start);
-  sim.results = senc_sim_fun(sim.matrix=sim.matrix,sim=sim)
-  sim.end = date(); print(sim.end);
-  write.csv(sim.results$all.populations,paste('SENC_all.pops_sim',sim,'.csv',sep=''), quote=F,row.names=F)
-  write.csv(sim.results$time.richness,paste("SENC_time.rich_sim",sim,".csv",sep=""),quote=F,row.names=F);
-  write.tree(sim.results$phylo.out,paste("SENC_phylo_sim",sim,".tre",sep=""));
-  write.csv(sim.results$params.out,paste("SENC_params.out_sim",sim,".csv",sep=""),quote=F,row.names=F);
+  #(4) call simulation, output is written to the raw_sim_output folder
+  sim.start = date()
+  print(sim.start)
+  sim.results = senc_sim_fun(sim.matrix = sim.matrix, sim = sim)
+  sim.end = date()
+  print(sim.end)
+  write.csv(sim.results$all.populations,paste("raw_sim_output/SENC_all.pops_sim", sim, ".csv", sep = ""), quote = F, row.names = F)
+  write.csv(sim.results$time.richness,paste("raw_sim_output/SENC_time.rich_sim", sim, ".csv", sep = ""), quote = F, row.names = F)
+  write.tree(sim.results$phylo.out,paste("raw_sim_output/SENC_phylo_sim", sim, ".tre", sep = ""))
+  write.csv(sim.results$params.out,paste("raw_sim_output/SENC_params.out_sim", sim, ".csv", sep = ""), quote = F, row.names = F)
   
 }
