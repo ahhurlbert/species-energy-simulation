fbe828cc04ef9a9fbab8159daa762dd5431d4c88
ahhurlbert <hurlbert@bio.unc.edu>
2013-11-21 10:11:34 -0500

    if raw simulation output data are not zipped, will read in appropriate raw files

--- code/unzipping_files.r
+++ code/unzipping_files.r
@@ -2,23 +2,27 @@
 #Function for unzipping output files, and storing them in a list
 # --output_dir is the directory where the zipped output files reside
 
-output.unzip = function(sim_dir, sim_ID) {
+output.unzip = function(raw_sim_output, sim_ID) {
   require(ape)
   
   
-  #check that file exists (returns value of 0 when mode=0 if file exists)
-  if( file.access(paste(sim_dir,'/senc.out.',sim_ID,'.zip',sep=''), mode=0) == 0 ) {
-    unzipped.filenames = unzip(paste(sim_dir,'/senc.out.',sim_ID,'.zip',sep=''), exdir = sim_dir)
+  #check that zip file exists (returns value of 0 when mode=0 if file exists)
+  if( file.access(paste(raw_sim_output,'/senc.out.',sim_ID,'.zip',sep=''), mode=0) == 0 ) {
+    unzipped.filenames = unzip(paste(raw_sim_output,'/senc.out.',sim_ID,'.zip',sep=''), exdir = raw_sim_output)
     all.populations = read.csv(unzipped.filenames[1], header=T)
     params.out = read.csv(unzipped.filenames[2],header=T)
     phylo.out = read.tree(unzipped.filenames[3])
     time.richness = read.csv(unzipped.filenames[4], header=T)
-    sim.results = list(all.populations=all.populations, 
-                       params.out=params.out, 
-                       phylo.out=phylo.out, 
-                       time.richness=time.richness)
-    return(sim.results)
-  } else { 
-    return(NULL)}
+  } else { #if sim output data are not zipped, then just read in raw files
+    all.populations = read.csv(paste(raw_sim_output, '/SENC_all.pops_sim', sim_ID, '.csv', sep = ''), header=T)
+    params.out = read.csv(paste(raw_sim_output, '/SENC_params.out_sim', sim_ID, '.csv', sep = ''), header=T)
+    phylo.out = read.tree(paste(raw_sim_output, '/SENC_phylo_sim', sim_ID, '.tre', sep = ''))
+    time.richness = read.csv(paste(raw_sim_output, '/SENC_time.rich_sim', sim_ID, '.csv', sep = ''), header=T)
+  }
+  sim.results = list(all.populations=all.populations, 
+                     params.out=params.out, 
+                     phylo.out=phylo.out, 
+                     time.richness=time.richness)
+  return(sim.results)
 }
 
