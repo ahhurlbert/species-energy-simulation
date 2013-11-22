529e5b90dbd41bcad6dab08edc1a70a222e6f680
ahhurlbert <hurlbert@bio.unc.edu>
2013-11-22 08:26:21 -0500

    got rid of semicolons

--- code/analysis_workflow_Olympus.r
+++ code/analysis_workflow_Olympus.r
@@ -1,28 +1,28 @@
 #!/usr/bin/env Rscript
 
 if (local == 0) {
-  sim = commandArgs();
-  sim = as.numeric(sim[length(sim)]);
+  sim = commandArgs()
+  sim = as.numeric(sim[length(sim)])
 }
   
 # Choose number of time slices per simulation to analyze
-num.of.time.slices = 1; # use -999 if you want to define specific time slices
+num.of.time.slices = 1 # use -999 if you want to define specific time slices
 
 # which.time.slices is (apparently) only for specifying particular, unevenly spaced time slices;
 # if not being used it should be set to -999
-which.time.slices = -999;
+which.time.slices = -999
 
 # time.sequence is (apparently) for when the time slices occur for a regular interval; set to -999 if not being used
 # Note that due to the slow calculation of tree imbalance (beta) for large trees, it may be best to specify only ~20 time slices
-time.sequence = -999; # to turn off 'time.sequence'
-#time.sequence = seq(2,300,by=2); # for time scenario sims
-#time.sequence = seq(1000,100000,length=100); # for energy gradient sims
+time.sequence = -999 # to turn off 'time.sequence'
+#time.sequence = seq(2,300,by=2) # for time scenario sims
+#time.sequence = seq(1000,100000,length=100) # for energy gradient sims
 
 # choose root only or all clades
 root.only = 0 # 0 means all clades, 1 means just the root
 
 # Set minimum number of species in a clade needed to proceed with analysis
-min.num.spp = 8;
+min.num.spp = 8
 
 # Specify number of clusters available for parallel proccessing
 num.clusters = 2
@@ -46,20 +46,20 @@ library(apTreeshape)
 library(foreach)
 library(doParallel)
 
-package.vector = c('ape','permute','nlme','vegan','picante','mvtnorm','caper','paleotree','plyr','phytools','apTreeshape');
+package.vector = c('ape','permute','nlme','vegan','picante','mvtnorm','caper','paleotree','plyr','phytools','apTreeshape')
 
-source('code/reg_calc_and_analysis.r');
-source('code/make.phylo.jimmy.fun.r');
-source('code/lat.grad.time.plot.r');
-source('code/clade.origin.corr.plot.r');
-source('code/clade.exmpl.figs.r');
-source('code/extinct.calc.r');
-source('code/unzipping_files.r');
-source('code/maxlik.betasplit.AH.r');
+source('code/reg_calc_and_analysis.r')
+source('code/make.phylo.jimmy.fun.r')
+source('code/lat.grad.time.plot.r')
+source('code/clade.origin.corr.plot.r')
+source('code/clade.exmpl.figs.r')
+source('code/extinct.calc.r')
+source('code/unzipping_files.r')
+source('code/maxlik.betasplit.AH.r')
 
 if (local == 1) {
-  cl = makeCluster(num.clusters);
-  registerDoParallel(cl);
+  cl = makeCluster(num.clusters)
+  registerDoParallel(cl)
 }
 
 #(3) read in master simulation matrix with chosen parameter combinations;
@@ -72,7 +72,7 @@ sim.matrix$skipped.clades = NA
 sim.matrix$skipped.times = NA
 
   rm(list=c('all.populations', 'time.richness', 'phylo.out', 'params.osput', 'output', 'sim.results'))
-  output = numeric();
+  output = numeric()
   
   # (5) read in simulation results for specified simulation from the output zip file, or the raw output files
   sim.results = output.unzip('raw_sim_output', sim)
@@ -83,13 +83,13 @@ sim.matrix$skipped.times = NA
     phylo.out = sim.results$phylo.out
     params.out = sim.results$params.out
     
-    max.time.actual = max(time.richness$time);
+    max.time.actual = max(time.richness$time)
     # If just a single timeslice, then use the end of the simulation or a designated time, otherwise space them equally (which.time.slices == -999)
     # or use specified vector in which.time.slices
     if (num.of.time.slices == 1) { 
       timeslices = max.time.actual 
     } else {
-      if (which.time.slices != -999 & num.of.time.slices == - 999) { timeslices = which.time.slices };
+      if (which.time.slices != -999 & num.of.time.slices == - 999) { timeslices = which.time.slices }
       if (which.time.slices == -999 & num.of.time.slices > 1) {
         timeslices = as.integer(round(seq(max(time.richness$time) / num.of.time.slices,
                                           max(time.richness$time), length = num.of.time.slices), digits = 0))
@@ -102,13 +102,13 @@ sim.matrix$skipped.times = NA
     skipped.times = ""
     for (t in timeslices) {
       # vector of species in existence at time t
-      sub.species = as.character(unique(subset(all.populations, time.of.origin <= t & time.of.extinction > t, select = 'spp.name'))[,1]);
+      sub.species = as.character(unique(subset(all.populations, time.of.origin <= t & time.of.extinction > t, select = 'spp.name'))[,1])
       
       # Some species may be extant globally (extant==1) but in our boundary regions (0 or 11) only;
       # we need to eliminate species that are not extant within regions 1-10 (which is all that is
       # reflected in the all.populations dataframe)
-      time.slice.populations = all.populations;
-      time.slice.populations$extant = 0;
+      time.slice.populations = all.populations
+      time.slice.populations$extant = 0
       time.slice.populations$extant[time.slice.populations$time.of.origin <= t & time.slice.populations$time.of.extinction > t] = 1
       extant.ornot = aggregate(time.slice.populations$extant, by = list(time.slice.populations$spp.name), sum)
       extinct.species = as.character(extant.ornot[extant.ornot$x == 0, 'Group.1'])
@@ -116,19 +116,20 @@ sim.matrix$skipped.times = NA
       sub.species2 = sub.species[!sub.species %in% extinct.species] # 'true' list of extant species
       tips.to.drop = as.character(phylo.out$tip.label[!phylo.out$tip.label %in% sub.species2]) # species to drop
       
-      # check to see if there are at least min.num.spp species for continuing with the analysis; if not store the skipped timeslice
+      # check to see if there are at least min.num.spp species for continuing with the analysis; 
+      # if not store the skipped timeslice
       if ( (length(phylo.out$tip.label) - length(tips.to.drop)) < min.num.spp) {
         skipped.times = paste(skipped.times, t) # keep track of the timeslices that were skipped in a text string
       } else {
         
         # Drop extinct species out of the phylogeny and tidy up
-        sub.phylo = drop.tip(phylo.out, tips.to.drop);
+        sub.phylo = drop.tip(phylo.out, tips.to.drop)
         temp.root.time = max(dist.nodes(sub.phylo)[1:Ntip(sub.phylo), Ntip(sub.phylo) + 1])
         most.recent.spp = sub.phylo$tip.label[as.numeric(names(which.max(dist.nodes(sub.phylo)[1:Ntip(sub.phylo), Ntip(sub.phylo) + 1])))]
         extinct.time.most.recent = unique(all.populations$time.of.sp.extinction[all.populations$spp.name == most.recent.spp])
         sub.phylo$root.time = temp.root.time + max(c(0, max.time.actual - extinct.time.most.recent))
-        sub.phylo = collapse.singles(timeSliceTree(sub.phylo, sliceTime = (max.time.actual - t), plot = F, drop.extinct = T));
-        num.of.spp = length(sub.phylo$tip.label);
+        sub.phylo = collapse.singles(timeSliceTree(sub.phylo, sliceTime = (max.time.actual - t), plot = F, drop.extinct = T))
+        num.of.spp = length(sub.phylo$tip.label)
         
         
         # The 'c' loop repeats the basic analysis for every node in the tree (if root.only = 0)
@@ -139,20 +140,21 @@ sim.matrix$skipped.times = NA
           
           #pull out list of species names belonging to each subclade
           sub.clade = clade.members(c, sub.phylo, tip.labels=T)
-          subset.populations = subset(all.populations, spp.name %in% as.numeric(sub.clade));
+          subset.populations = subset(all.populations, spp.name %in% as.numeric(sub.clade))
           
           #sub.populations is the subset of populations specific to a particular clade and timeslice
           sub.populations = subset(subset.populations, time.of.origin <= t & time.of.extinction > t)
           
           #sub.clade.phylo is a specific simulation clade pulled from the phylogeny that was sliced at timeslice t
-          tips.to.drop2 = as.character(sub.phylo$tip.label[which(is.element(sub.phylo$tip.label, as.character(sub.populations$spp.name)) == F)]);
+          tips.to.drop2 = as.character(sub.phylo$tip.label[which(is.element(sub.phylo$tip.label, as.character(sub.populations$spp.name)) == F)])
           
-          # check to see if there are at least min.num.spp species for continuing with the analysis; if not increment skipped.clades
+          # check to see if there are at least min.num.spp species for continuing with the analysis
+          # if not increment skipped.clades
           if((length(sub.phylo$tip.label) - length(tips.to.drop2)) < min.num.spp) {
             skipped.clades = skipped.clades + 1
           } else {
             
-            sub.clade.phylo = drop.tip(sub.phylo, tips.to.drop2);
+            sub.clade.phylo = drop.tip(sub.phylo, tips.to.drop2)
             sub.clade.phylo$root.time = max(dist.nodes(sub.clade.phylo)[1:Ntip(sub.clade.phylo), Ntip(sub.clade.phylo) + 1])
             sub.clade.phylo$origin.time = t - sub.clade.phylo$root.time
             
@@ -164,7 +166,7 @@ sim.matrix$skipped.times = NA
             # Calculate summary statistics at the regional level
             reg.summary = regional.calc(sub.populations[,c('region','spp.name','time.of.origin','reg.env','extant')], 
                                         sub.clade.phylo, 
-                                        as.integer(t));
+                                        as.integer(t))
             
             #Note that extinction calculation must be done on subset.populations, not sub.populations
             extinction = extinct.calc(subset.populations, timeslice = t)
@@ -210,12 +212,12 @@ sim.matrix$skipped.times = NA
             output = rbind(output, cbind(sim = sim, clade.id = c, time = t, corr.results, gamma.stat = Gamma.stat,
                                          clade.richness = length(unique(sub.populations$spp.name)), 
                                          tree.beta = tree.beta))
-            print(paste(sim, sub.clade.loop.end, c, t, date(), length(sub.clade.phylo$tip.label), sep="   "));
-            flush.console();
+            print(paste(sim, sub.clade.loop.end, c, t, date(), length(sub.clade.phylo$tip.label), sep="   "))
+            flush.console()
           } # end third else
         } # end sub clade for loop
       } # end second else
-    }; # end timeslice loop
+    } # end timeslice loop
 
     # this print statement should show up in the output files on the cluster.
     print(warnings())
@@ -228,7 +230,7 @@ sim.matrix$skipped.times = NA
       write.csv(output, paste("Stats_sim", sim, "_rootclade_only.csv", sep = ""), quote = F, row.names = F)
     }
     
-    analysis.end = date();
+    analysis.end = date()
     
     # Add overall summary info
     sim.matrix[sim.matrix$sim.id == sim, 'n.regions'] = length(unique(all.populations$region))
@@ -237,6 +239,6 @@ sim.matrix$skipped.times = NA
     sim.matrix[sim.matrix$sim.id == sim, 'skipped.clades'] = skipped.clades # number of clades skipped over for analysis, summed over timeslices
     sim.matrix[sim.matrix$sim.id == sim, 'skipped.times'] = skipped.times # number of time slices skipped over for analysis
     
-    write.csv(sim.matrix[sim.matrix$sim.id == sim,], paste("analysis_output/summary_output_sim", sim, ".csv", sep = ""), quote = F, row.names = F);
+    write.csv(sim.matrix[sim.matrix$sim.id == sim,], paste("analysis_output/summary_output_sim", sim, ".csv", sep = ""), quote = F, row.names = F)
   } # end first if (file check)
 
