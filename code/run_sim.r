80e456c9599030510d5b277ad73c3145870b88c9
ahhurlbert <hurlbert@bio.unc.edu>
2013-12-05 14:48:51 -0500

    new wrapper function for running simulations; still need to write code for when local==F

---
+++ code/run_sim.r
@@ -0,0 +1,42 @@
+# Use this function to run simulations by specifying:
+#   which_sims:  the simulation IDs to run
+#   sim_matrix:  a .csv file specifying parameter combinations for each sim ID
+#   local:       TRUE if running on a local machine, FALSE if running on a remote cluster
+#   num_cores:   number of processors available for parallel processing
+
+run_sim = function(which_sims, 
+                   sim_matrix_filename = "SENC_Master_Simulation_Matrix.csv", 
+                   local = T, 
+                   num_cores = 1) 
+{
+
+  require(ape)
+  require(parallel)
+  require(doParallel)
+  
+  cl = makeCluster(num_cores)
+  registerDoParallel(cl)
+  
+  source("code/senc_sim_fun.r")
+
+  sim_matrix = read.csv(sim_matrix_filename, header = T)
+
+  if(local) {
+    foo = foreach(sim = which_sims, .packages = 'ape', .combine = "rbind") %dopar% {
+      
+      sim_start = date()
+      print(sim_start)
+      sim.results = senc_sim_fun(sim_matrix, sim = sim)
+      sim_end = date()
+      print(sim_end)
+      
+    } #end foreach
+    
+  } #end if(local)
+
+  if(!local) {
+    #Pass in arguments from shell command line? James, is something like this necessary?
+  } #end if(!local)
+  
+} #end function
+
