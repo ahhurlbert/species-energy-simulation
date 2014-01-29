65644e6b9925ac47cce0a62bba6c555700b4a4ab
ahhurlbert <hurlbert@bio.unc.edu>
2014-01-29 10:03:03 -0500

    added code for parallelizing using doMPI for the non-local (i.e. cluster) case

--- code/run_sim.r
+++ code/run_sim.r
@@ -36,7 +36,30 @@ run_sim = function(which_sims,
   } #end if(local)
 
   if(!local) {
-    #Pass in arguments from shell command line? James, is something like this necessary?
+    library(doMPI)
+    
+    # create and register a doMPI cluster if necessary
+    if (!identical(getDoParName(), 'doMPI')) {
+      cl <- startMPIcluster()
+      registerDoMPI(cl)
+    }
+    
+    #Create a log file for checking simulation progress
+    writeLines(c(""), "raw_sim_output/log.txt")
+    
+    foo = foreach(sim = which_sims, .packages = 'ape', .combine = "rbind", .export =
+                    "senc_sim_fun")  %dopar% {
+                      
+      sink("raw_sim_output/log.txt", append = T)
+      cat(paste("Starting sim", sim, ",", date(), "\n"))
+      sim.results = senc_sim_fun(sim_matrix, sim = sim)
+                      
+    } #end foreach
+    
+    
+    closeCluster(cl);
+    mpi.finalize()
+    
   } #end if(!local)
   
 } #end function
