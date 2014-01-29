6d10b00f70e09d4d76555f90c19af48192ddc112
ahhurlbert <hurlbert@bio.unc.edu>
2014-01-29 13:14:24 -0500

    cleaned up

--- run_sim_on_cluster.r
+++ run_sim_on_cluster.r
@@ -1,16 +1,16 @@
-library(ape);
-library(permute);
-library(nlme);
-library(vegan);
-library(picante);
-library(mvtnorm);
-library(caper);
-library(paleotree);
-library(plyr);
-library(phytools);
-library(foreach);
-library(doParallel);
-library(apTreeshape);
+library(ape)
+library(permute)
+library(nlme)
+library(vegan)
+library(picante)
+library(mvtnorm)
+library(caper)
+library(paleotree)
+library(plyr)
+library(phytools)
+library(foreach)
+library(doParallel)
+library(apTreeshape)
 library(mnormt)
 library(rgl)
 library(apTreeshape)
@@ -24,13 +24,11 @@ source('code/senc_sim_fun.r')
 source('code/senc_analysis_fun.r')
 source('code/supplemental_analysis_functions.r')
 
-args = commandArgs()
+args = commandArgs(trailingOnly = TRUE)
 
 which_sims = c()
 for (i in 1:length(args)/2) {
-  which_sims = c(which_sims, as.numeric( args[2*i - 1] ) : as.numeric( argsN[2*i]) )
+  which_sims = c(which_sims, as.numeric( args[2*i - 1] ) : as.numeric( args[2*i]) )
 }
 
-sim_matrix = read.csv('test_matrix.csv', header = T)
-
-run_sim(which_sims, sim_matrix, local = F)
+run_sim(which_sims, "test_matrix.csv", local = F)
