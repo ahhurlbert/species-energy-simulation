0468f470e119daec71def33290acd5eca4ad0254
ahhurlbert <hurlbert@bio.unc.edu>
2014-01-30 21:44:19 -0500

    added comment describing how the command line arguments are being used to create a vector of sim.id's

--- run_sim_on_cluster.r
+++ run_sim_on_cluster.r
@@ -26,6 +26,10 @@ source('code/supplemental_analysis_functions.r')
 
 args = commandArgs(trailingOnly = TRUE)
 
+# Specify a vector of sim.id's to run in the command line call
+# --if a simple sequence, then arguments are simply the first and last sims
+# --if more complex set, then arguments are paired to represent subsequences, and then catenated
+# --e.g. R script run_sim_on_cluster.r 1 18 26 29  would run c(1:18, 26:29)
 which_sims = c()
 for (i in 1:(length(args)/2)) {
   which_sims = c(which_sims, as.numeric( args[2*i - 1] ) : as.numeric( args[2*i]) )
