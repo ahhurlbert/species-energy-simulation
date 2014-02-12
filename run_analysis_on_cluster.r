ea716263cb309f4ea911c87ebd30af135d4de295
ahhurlbert <hurlbert@bio.unc.edu>
2014-02-12 08:56:47 -0500

    added passing of number of time slices to analyze via the shell script

--- run_analysis_on_cluster.r
+++ run_analysis_on_cluster.r
@@ -1,9 +1,9 @@
 # R script to be called from the shell that will run analysis on the specified simulations
 # Typical call on the UNC KillDevil cluster (which uses LSF) would be
 #
-#   $bsub -o out.%J -n 100 -a openmpi mpirun Rscript run_analysis_on_cluster.r 3765 3864
+#   $bsub -o out.%J -n 100 -a openmpi mpirun Rscript run_analysis_on_cluster.r 3765 3864 30
 #
-# which would use 100 nodes to analyze sims 3765:3864 
+# which would use 100 nodes to analyze sims 3765:3864 at 30 evenly spaced time points
 
 library(ape)
 library(permute)
@@ -36,8 +36,10 @@ args = commandArgs(trailingOnly = TRUE)
 # --if more complex set, then arguments are paired to represent subsequences, and then catenated
 # --e.g. R script run_sim_on_cluster.r 1 18 26 29  would run c(1:18, 26:29)
 which_sims = c()
-for (i in 1:(length(args)/2)) {
+for (i in 1:((length(args) - 1)/2)) {
   which_sims = c(which_sims, as.numeric( args[2*i - 1] ) : as.numeric( args[2*i]) )
 }
 
-analyze_sim(which_sims, "SENC_Master_Simulation_Matrix.csv", local = F, sim_dir = "new")
+times = args[length(args)]
+
+analyze_sim(which_sims, "SENC_Master_Simulation_Matrix.csv", local = F, sim_dir = "new", num.of.time.slices = times)
