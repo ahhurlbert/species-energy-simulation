84445a263c80b57e1d4befba55b87e13250114d3
Allen Hurlbert <hurlbert@bio.unc.edu>
2014-05-09 16:20:21 -0400

    for calculating distributions of range size, age, and abundance at a given point in time

---
+++ code/range_age_abundance.r
@@ -0,0 +1,17 @@
+source('z:/git/species-energy-simulation/code/supplemental_analysis_functions.r')
+source('z:/git/species-energy-simulation/code/abundance_thru_time.r')
+sim4065 = output.unzip('z:/sencoutput/hurlbert_and_stegen_2014/raw_sim_output', 4065)
+all.pops = sim4065$all.populations
+extant.pops = subset(all.pops, extant==1)
+
+#Range size distributions (trivial since few species occur outside one bin)
+range.sizes = data.frame(table(extant.pops$spp.name))
+names(range.sizes) = c('spp.name', 'rangesize')
+
+#Age distributions
+ages.mean = aggregate(extant.pops$time.of.sp.origin, by = list(extant.pops$region), function(x) 
+                  (mean(max(extant.pops$time.of.extinction) - x)))
+ages.sd = aggregate(extant.pops$time.of.sp.origin, by = list(extant.pops$region), function(x) 
+  (var(max(extant.pops$time.of.extinction) - x))^0.5)
+
+#Abundance distributions
