d475a0c6671240fcaf1d12ec548477d057afec30
Allen Hurlbert <hurlbert@bio.unc.edu>
2020-09-08 13:21:02 -0400

    adding test files

---
+++ test_fun_on_cluster_nonparallel.r
@@ -0,0 +1,18 @@
+# R script to be called from the shell that will run the specified simulations
+
+# Test running on cluster, but not in parallel
+
+source('test_fun.r')
+
+args = commandArgs(trailingOnly = TRUE)
+
+# Specify a vector of sim.id's to run in the command line call
+# --if a simple sequence, then arguments are simply the first and last sims
+# --if more complex set, then arguments are paired to represent subsequences, and then catenated
+# --e.g. R script run_sim_on_cluster.r 1 18 26 29  would run c(1:18, 26:29)
+which_sims = c()
+for (i in 1:(length(args)/2)) {
+  which_sims = c(which_sims, as.numeric( args[2*i - 1] ) : as.numeric( args[2*i]) )
+}
+
+test_fun(which_sims, "test_matrix.csv", local = T)
